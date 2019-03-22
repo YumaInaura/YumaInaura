f49fcdc9094251f39353c2180e3cb53e899eef1a
Yumainaura <yuma.inaura@gmail.com>
2019-03-23 07:06:22 +0900

    modified:   twitter/create.py

--- twitter/create.py
+++ twitter/create.py
@@ -1,6 +1,6 @@
 # https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/post-statuses-update
 
-import json, config, os, re, urllib.parse
+import json, config, os, re, urllib.parse, unicodedata, sys
 from requests_oauthlib import OAuth1Session
  
 CK = config.CONSUMER_KEY
@@ -9,15 +9,39 @@ AT = config.ACCESS_TOKEN
 ATS = config.ACCESS_TOKEN_SECRET
 twitter = OAuth1Session(CK, CS, AT, ATS)
 
-formatted_message = re.sub(r'\\n', "\n", os.environ.get("MESSAGE"))
+def twitter_length(text):
+    count = 0
+    for c in text:
+        if unicodedata.east_asian_width(c) in 'FWA':
+            count += 2
+        else:
+            count += 1
+    return count
+
+message = ''
+
+for line in sys.stdin:
+  message += re.sub(r'^\s+|\s+$', '', line)
+
+formatted_message = re.sub(r'\\n', "\n", message)
+
+message_length = twitter_length(formatted_message)
+
 formatted_message = formatted_message[:140]
+
 encoded_message = urllib.parse.quote_plus(formatted_message)
 
-url = "https://api.twitter.com//1.1/statuses/update.json?status={message}".format(**{ "message" : encoded_message })
+tweet = {
+  "status" : encoded_message,
+  "in_reply_to_status_id" : os.environ.get('IN_REPLY_TO_STATUS_ID') if os.environ.get('IN_REPLY_TO_STATUS_ID') else '',
+  "auto_populate_reply_metadata" : 'true',
+}
+
+url = "https://api.twitter.com//1.1/statuses/update.json?status={status}&in_reply_to_status_id={in_reply_to_status_id}&auto_populate_reply_metadata={auto_populate_reply_metadata}".format(**tweet)
 
 if os.environ.get("DEBUG"):
-  print(encoded_message)
+  print(url)
 else:
   res = twitter.post(url)
-  print(res.json())
+  print(json.dumps(res.json()))
 
