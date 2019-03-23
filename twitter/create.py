75e135104c44ed65bdab6472cd8add76aa9637c6
Yumainaura <yuma.inaura@gmail.com>
2019-03-23 16:14:52 +0900

    modified:   twitter/create.py new file:   twitter/split.py

--- twitter/create.py
+++ twitter/create.py
@@ -9,15 +9,6 @@ AT = config.ACCESS_TOKEN
 ATS = config.ACCESS_TOKEN_SECRET
 twitter = OAuth1Session(CK, CS, AT, ATS)
 
-def twitter_length(text):
-    count = 0
-    for c in text:
-        if unicodedata.east_asian_width(c) in 'FWA':
-            count += 2
-        else:
-            count += 1
-    return count
-
 message = ''
 
 for line in sys.stdin:
