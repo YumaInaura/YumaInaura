5506e729494e39bfd7b7cfd0408028b0717ef5d5
Yumainaura <yuma.inaura@gmail.com>
2019-03-26 08:50:21 +0900

    modified:   twitter/create.sh

--- twitter/create.sh
+++ twitter/create.sh
@@ -7,5 +7,6 @@ do
   result=$(echo "$line" | IN_REPLY_TO_STATUS_ID="$status_id" python3 twitter/create.py)
   echo "$result"
   status_id=$(echo "$result" | jq --raw-output '.["id_str"]')
+  sleep 15
 done 
 
