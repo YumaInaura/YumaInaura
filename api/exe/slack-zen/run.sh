f4fd89f3162790745f694c0b3ba548b7be520bc1
Yumainaura <yuma.inaura@gmail.com>
2019-03-28 13:36:04 +0900

    modified:   api/exe/slack-zen/run.sh

--- api/exe/slack-zen/run.sh
+++ api/exe/slack-zen/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 basedir=$(dirname "$0")
-source ${basedir}/../../setting.sh
-api_dir=${base_dir}/../../lib
+source ${basedir}../../setting.sh
+api_dir=${base_dir}../../lib
 
 github_repository="playground"
 
@@ -64,7 +64,7 @@ fi
 echo "CREATE OR EDITED ISSUE"
 echo "$github_issue"
 
-pushd "$api_dir"
+#pushd "$api_dir"
   # echo "$slack_message" | python ./twitter/oneline-split.py | ./twitter/create.sh 
-popd
+#popd
 
