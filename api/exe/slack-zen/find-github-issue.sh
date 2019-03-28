b5cea460aca7d90f07070d671451bc544dfe3a16
Yumainaura <yuma.inaura@gmail.com>
2019-03-29 08:45:11 +0900

    modified:   api/exe/slack-zen/find-github-issue.sh

--- api/exe/slack-zen/find-github-issue.sh
+++ api/exe/slack-zen/find-github-issue.sh
@@ -5,27 +5,18 @@ set -eu
 basedir=$(dirname "$0")
 
 source "${basedir}/../../setting.sh"
-api_dir="${basedir}/../../lib"
-log_dir="${basedir}"/log
+source "${basedir}/prepare.sh"
 
 mkdir -p "$log_dir"
 rm -rf "$log_dir"/*github*
 
-date=$(TZ=Asia/Tokyo date +'%Y-%m-%d')
-github_title="いなうらゆうまはここにいた ${date}"
-
-github_repository="playground"
-
-issue_list_log_file="$log_dir"/github-issue.json
-found_issue_log_file="$log_dir"/github-found-issue.json
-
 OWNER=YumaInaura \
 REPOSITORY="$github_repository" \
   python "$api_dir"/github/issue.py \
-    | tee "$issue_list_log_file"
+    | tee "$github_issue_list_log_file"
 
-cat "$issue_list_log_file" | \
+cat "$github_issue_list_log_file" | \
   jq '.[]' | \
   jq -c 'select(.["title"] | contains("'"$github_title"'"))' \
-  | tee "$found_issue_log_file"
+  | tee "$github_found_issue_log_file"
 
