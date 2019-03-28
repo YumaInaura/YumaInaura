e2514dfd76f5c618fc3ded4c4c5f57b47134efe9
Yumainaura <yuma.inaura@gmail.com>
2019-03-29 07:56:41 +0900

    modified:   api/exe/slack-zen/slack-message-to-log.sh

--- api/exe/slack-zen/slack-message-to-log.sh
+++ api/exe/slack-zen/slack-message-to-log.sh
@@ -13,7 +13,12 @@ source "${basedir}/../../setting.sh"
 api_dir="${basedir}/../../lib"
 log_dir="${basedir}"/log
 
+channel_history_log_file="$log_dir"/slack-channel-history.json
+message_log_file="$log_dir"/slack-message.json
+user_message_log_file="$log_dir"/slack-user-message.json
+
 mkdir -p "${log_dir}"
+rm -rf "${log_dir}"/*
 
 date=$(TZ=Asia/Tokyo date +'%Y-%m-%d')
 github_title="いなうらゆうまはここにいた ${date}"
@@ -22,12 +27,12 @@ github_repository="playground"
 
 # --------------------------------------
 
-eval "${basedir}/channel-history.sh" > "$log_dir"/channel-message.json
+eval "${basedir}/channel-history.sh" > "$channel_history_log_file"
 
-cat "$log_dir"/channel-message.json | jq '.["messages"][]' > "$log_dir"/slack-message.json
-cat "$log_dir"/slack-message.json | jq 'select(has("client_msg_id"))' > "$log_dir"/user-slack-message.json
+cat "$channel_history_log_file" | jq '.["messages"][]' > "$message_log_file"
+cat "$message_log_file" | jq 'select(has("client_msg_id"))' > "$user_message_log_file"
 
-user_slack_messages=$(cat "$log_dir"/user-slack-message.json)
+user_slack_messages=$(cat "$user_message_log_file")
 
 if [ "$user_slack_messages" == "[]" ] || [ -z "$user_slack_messages" ]; then
   echo No slack messages found
