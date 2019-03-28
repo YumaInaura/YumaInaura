b0f9e97d737b8015d31494e003db88d01c0f74a9
Yumainaura <yuma.inaura@gmail.com>
2019-03-28 17:50:41 +0900

    new file:   api/exe/twitter/twitter-to-github-diary.sh

---
+++ api/exe/twitter/twitter-to-github-diary.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -eu
+
+export LC_CTYPE=en_US.UTF-8
+
+basedir=$(dirname "$0")
+
+source ~/.secret/setting.sh
+source "${basedir}/../../setting.sh"
+api_dir="${basedir}/../../lib"
+
+cd "$api_dir"
+
+pushd ${api_dir}/twitter
+  mkdir -p log
+  INCLUDE_RTS=1 INCLUDE_REPLIES=1 python timeline.py > log/timeline.log
+  cat log/timeline.log | python jst-datetime-filter.py > log/timeline-jst-yesterday.log
+  cat log/timeline-jst-yesterday.log | python markdown.py > log/markdown.log
+popd
+
+date=$(TZ=TZ=Asia/Tokyo date --date="1 days ago" +'%Y-%m-%d')
+
+#export USERNAME=YumaInaura \
+#  REPOSITORY=YumaInaura \
+#  PASSWORD="$github_api_key" \
+#  TITLE="いなうらゆうま はここにいた ${date} on Twitter" \
+#  FILE=./twitter/log/markdown.log \
+#  LABELS=medium,hatena,japanese,twitter
