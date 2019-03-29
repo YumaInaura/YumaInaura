f83bec4dbea1e8b08beccbafffee51d270829b7c
Yumainaura <yuma.inaura@gmail.com>
2019-03-29 10:47:35 +0900

    modified:   api/exe/slack-zen/github-create-or-append-daily-issue.sh

--- api/exe/slack-zen/github-create-or-append-daily-issue.sh
+++ api/exe/slack-zen/github-create-or-append-daily-issue.sh
@@ -11,23 +11,15 @@ basedir=$(dirname "$0")
 source "${basedir}/../../setting.sh"
 source "${basedir}/prepare.sh"
 
-set -x
-
 slack_message="# test\nbody"
-github_found_top_issue=$(cat "$github_found_issue_log_file")
+github_found_top_issue=$(cat "$github_found_top_issue_log_file")
 
 echo "$github_found_top_issue"
 
-exit
-
-if [[ ! -z "$github_found_top_issue" && "$github_found_top_issue" -ne "null" ]]; then
-  echo a
-  echo "$github_found_top_issue"
-  exit
-
+if [[ ! -z "$github_found_top_issue" ]]; then
   title=$(echo "$github_found_top_issue" | jq --raw-output '.title')
   body=$(echo "$github_found_top_issue" | jq --raw-output '.body')$slack_message
-  issue_number=$(echo "$found_top_issue" | jq '.["number"]')
+  issue_number=$(echo "$github_found_top_issue" | jq '.["number"]')
  
   github_issue=$(
     USERNAME=YumaInaura \
@@ -39,8 +31,6 @@ if [[ ! -z "$github_found_top_issue" && "$github_found_top_issue" -ne "null" ]];
     python "$api_dir"/github/create-or-edit-issue.py
   )
 else
-  echo b
-  exit
   body=$slack_message
 
   github_issue=$(
