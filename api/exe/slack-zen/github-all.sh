b0afa8046a7c4227486dd1d0853bda08045b4254
Yumainaura <yuma.inaura@gmail.com>
2019-03-29 10:55:36 +0900

    modified:   api/exe/slack-zen/github-all.sh

--- api/exe/slack-zen/github-all.sh
+++ api/exe/slack-zen/github-all.sh
@@ -6,5 +6,5 @@ basedir=$(dirname "$0")
 
 "$basedir"/github-issue-list.sh && \
 "$base_dir"/github-filter-issue.sh && \
-"${base_dir}"/github-create-or-append-daily-issue.sh
+"${base_dir"/github-create-or-append-daily-issue.sh
 
