26dcf4b768f68436b2afd8417779c9871408d24c
Yumainaura <yuma.inaura@gmail.com>
2019-03-29 20:45:27 +0900

    modified:   api/exe/slack-zen/github-all.sh 	modified:   api/exe/slack-zen/github-create-or-append-daily-issue.sh

--- api/exe/slack-zen/github-all.sh
+++ api/exe/slack-zen/github-all.sh
@@ -3,6 +3,14 @@
 set -eu
 
 basedir=$(dirname "$0")
+source "${basedir}/prepare.sh"
+
+slack_message=$(cat "$markdown_text_log_file")
+
+if [ -z "$slack_message" ]; then
+  echo Slack message is empty Nothing to do
+  exit
+fi
 
 "$basedir"/github-issue-list.sh && \
 "$basedir"/github-filter-issue.sh && \
