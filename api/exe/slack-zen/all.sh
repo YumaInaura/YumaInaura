48bfa456a777bc69a05bc846d2ced94132012681
Yumainaura <yuma.inaura@gmail.com>
2019-03-30 12:37:43 +0900

    f

--- api/exe/slack-zen/all.sh
+++ api/exe/slack-zen/all.sh
@@ -4,7 +4,9 @@ set -eu
 
 export LC_CTYPE=en_US.UTF-8
 
+basedir=$(dirname "$0")
+
 "$basedir"/slack-all.sh && \
-"$basedir"/translate-all.sh && \
+"$basedir"/format-all.sh && \
 "$basedir"/github-all.sh
 
