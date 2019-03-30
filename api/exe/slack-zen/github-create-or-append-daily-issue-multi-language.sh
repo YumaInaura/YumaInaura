cbfe9b2e7aea0b4332a23c9c487ef49ca67f3575
Yumainaura <yuma.inaura@gmail.com>
2019-03-30 13:00:18 +0900

    modified:   api/exe/slack-zen/github-create-or-append-daily-issue-multi-language.sh

--- api/exe/slack-zen/github-create-or-append-daily-issue-multi-language.sh
+++ api/exe/slack-zen/github-create-or-append-daily-issue-multi-language.sh
@@ -7,5 +7,6 @@ export LC_CTYPE=en_US.UTF-8
 basedir=$(dirname "$0")
 source "${basedir}/prepare.sh"
 
+cat "$google_translate_en_log_file" | "${basedir}"/github-create-or-append-daily-issue.sh
 cat "$markdown_text_log_file" | "${basedir}"/github-create-or-append-daily-issue.sh
 
