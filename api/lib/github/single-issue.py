841f678b3ef4a336b2270d30bc0c409adda4ff3c
Yumainaura <yuma.inaura@gmail.com>
2019-03-30 15:45:11 +0900

    new file:   api/lib/github/single-issue.py

---
+++ api/lib/github/single-issue.py
@@ -0,0 +1,12 @@
+import requests, os, json
+
+owner = os.environ.get('OWNER')
+repository = os.environ.get('REPOSITORY')
+issue_number = os.environ.get('NUMBER')
+
+api_url = 'https://api.github.com/repos/' + owner + '/' + repository + '/issues/' + issue_number
+
+res = requests.get(api_url)
+result = res.json()
+
+print(json.dumps(result))
