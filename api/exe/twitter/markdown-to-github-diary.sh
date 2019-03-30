9d39bb8f67781da166827ebe8c91fbea9638b406
Yumainaura <yuma.inaura@gmail.com>
2019-03-31 07:26:25 +0900

    modified:   api/exe/twitter/markdown-to-github-diary.sh

--- api/exe/twitter/markdown-to-github-diary.sh
+++ api/exe/twitter/markdown-to-github-diary.sh
@@ -11,9 +11,9 @@ jst_date=$(TZ=Asia/Tokyo date --date='1 days ago' +'%Y-%m-%d')
 
 api_dir="${basedir}/../../lib"
 
-export USERNAME=YumaInaura \
+export OWNER=YumaInaura \
        REPOSITORY=YumaInaura \
-       PASSWORD="$github_api_key" \
+       API_KEY="$github_api_key" \
        TITLE="いなうらゆうま はここにいた ${jst_date} on Twitter" \
        FILE="${api_dir}/twitter/log/markdown.log" \
        LABELS=medium,hatena,japanese,twitter
