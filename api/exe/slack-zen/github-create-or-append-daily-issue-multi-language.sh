6b39d7d970c7cafc42a21804cbd12f79823eb5a5
Yumainaura <yuma.inaura@gmail.com>
2019-03-30 12:26:28 +0900

    new file:   api/exe/slack-zen/github-create-or-append-daily-issue-multi-language.sh

---
+++ api/exe/slack-zen/github-create-or-append-daily-issue-multi-language.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eu
+
+export LC_CTYPE=en_US.UTF-8
+
+basedir=$(dirname "$0")
+source "${basedir}/prepare.sh"
+
+slack_message=$(cat "$markdown_text_log_file")
+
+if [ -z "$slack_message" ]; then
+  echo Slack message is empty Nothing to do
+  exit
+fi
+
+"$basedir"/github-issue-list.sh && \
+"$basedir"/github-filter-issue.sh && \
+"$basedir"/github-create-or-append-daily-issue.sh
+
