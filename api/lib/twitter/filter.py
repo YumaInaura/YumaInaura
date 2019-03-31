7f8a347e7d7266d3701affeae542d3cea65c84b5
Yumainaura <yuma.inaura@gmail.com>
2019-04-01 01:00:16 +0900

    no named commit

---
+++ api/lib/twitter/filter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys, json
+
+timelines = json.loads(sys.stdin.read())
+
+OWN_USER_ID = 473780756
+
+  for result in timelines:
+    own_reply = True if result["in_reply_to_user_id"] == OWN_USER_ID else False
+    own_retweet = True if ('retweeted_status' in result and result["retweeted_status"]["user"]["id"] == OWN_USER_ID) else False
+
+    if os.environ.get("ONLY_REPLIES") and not result["in_reply_to_user_id"] and not own_reply:
+      continue
+
+    if not os.environ.get("ALL"):
+  
+      if result["in_reply_to_user_id"] and not own_reply:
+        continue
+  
+      if result["retweeted"] and not own_retweet:
+        continue
+
+
