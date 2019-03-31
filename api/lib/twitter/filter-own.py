f82fe19cf1222701dbedc575baf715bf9919dbf5
Yumainaura <yuma.inaura@gmail.com>
2019-04-01 01:27:46 +0900

    no named commit

--- api/lib/twitter/filter-own.py
+++ api/lib/twitter/filter-own.py
@@ -12,16 +12,14 @@ for result in timelines:
   own_reply = True if result["in_reply_to_user_id"] == OWN_USER_ID else False
   own_retweet = True if ('retweeted_status' in result and result["retweeted_status"]["user"]["id"] == OWN_USER_ID) else False
 
-  if os.environ.get("ONLY_REPLIES") and not result["in_reply_to_user_id"] and not own_reply:
+  if not result["in_reply_to_user_id"] and not own_reply:
     continue
 
-  if not os.environ.get("ALL"):
+  if result["in_reply_to_user_id"] and not own_reply:
+    continue
 
-    if result["in_reply_to_user_id"] and not own_reply:
-      continue
-
-    if result["retweeted"] and not own_retweet:
-      continue
+  if result["retweeted"] and not own_retweet:
+    continue
 
   results.append(result)
 
