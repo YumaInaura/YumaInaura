10cee5a85ff3039e6ba4adc828c647b293c6349e
Yumainaura <yuma.inaura@gmail.com>
2019-04-01 19:48:44 +0900

    no named commit

--- api/exe/slack-zen/slack-message-samurai.sh
+++ api/exe/slack-zen/slack-message-samurai.sh
@@ -5,16 +5,16 @@ set -eu
 basedir=$(dirname "$0")
 
 source "${basedir}/../../setting.sh"
-source "${basedir}/prepare-samurai.sh"
+source "${basedir}/prepare.sh"
 
 mkdir -p "${log_dir}"
 rm -rf "${log_dir}"/*slack*
 
-interval_sec=${INTERVAL:-$((60*60*24))}
+interval_sec=${INTERVAL:-60}
 oldest_unixtime=$(($(date +%s) - $interval_sec))
 
 TOKEN="$slack_token" \
-CHANNEL="CG8Q65EGP" \
+CHANNEL="$slack_channel_id" \
 OLDEST="$oldest_unixtime" \
   python "${basedir}"/../../lib/slack/channel-message.py \
    | tee "$slack_channel_history_log_file"
