d7b34841262aaef6f7ce04f13a17fe41a2a98356
Yumainaura <yuma.inaura@gmail.com>
2019-04-02 21:09:17 +0900

    no named commit

--- api/exe/slack-zen/prepare.sh
+++ api/exe/slack-zen/prepare.sh
@@ -1,3 +1,12 @@
+shopt -s expand_aliases
+
+if [ $(uname -s) = "Darwin" ]; then
+  alias date=gdate
+  alias sed=gsed
+fi
+
+alias python=python3
+
 basedir=$(dirname "$0")
 
 date=$(TZ=Asia/Tokyo date +'%Y-%m-%d')
--- api/setting.sh
+++ api/setting.sh
@@ -1,11 +1,2 @@
-shopt -s expand_aliases
-
-if [ $(uname -s) = "Darwin" ]; then
-  alias date=gdate
-  alias sed=gsed
-fi
-
-alias python=python3
-
 source ~/.secret/setting.sh
 
