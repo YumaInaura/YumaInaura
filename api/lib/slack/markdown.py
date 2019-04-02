900a0b407b7271cf15a79a40cdb9cb5b22a14b07
Yumainaura <yuma.inaura@gmail.com>
2019-04-02 12:23:12 +0900

    no named commit

--- api/lib/slack/markdown.py
+++ api/lib/slack/markdown.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import requests, os, json, datetime, sys
+import os, json, datetime, sys, re
 
 stdin_lines = sys.stdin.read()
 
@@ -9,8 +9,12 @@ messages = json.loads(stdin_lines)
 markdown_text = ''
 
 for message in messages:
-  
-  date = datetime.datetime.utcfromtimestamp(message['ts']).strftime('%Y-%m-%d %H:%M:%S')
-  markdown_text += date
+  text = message['text']
+  text = re.sub('^([^。]+)。', "# \\1。\n", text, 1)
+
+  markdown_text += text + "\n\n"
+
+  date = datetime.datetime.utcfromtimestamp(float(message['ts'])).strftime('%Y-%m-%d %H:%M:%S')
+  markdown_text += date + "\n\n"
 
 print(markdown_text)
