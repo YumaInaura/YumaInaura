5b38e89bf76e5a9dab57b506fbfdb691151b8b1b
Yumainaura <yuma.inaura@gmail.com>
2019-04-02 20:54:43 +0900

    no named commit

--- api/exe/slack-zen/samurai-all.sh
+++ api/exe/slack-zen/samurai-all.sh
@@ -9,8 +9,10 @@ export SETTING_FILE=samurai-setting.sh
 basedir=$(dirname "$0")
 source "$basedir"/prepare.sh
 
-"$basedir"/yesterday-slack-message.sh
+eval "$basedir"/yesterday-slack-message.sh
 cat "$slack_user_message_log_file" jq reverse | "$api_dir"/slack/markdown.py | tee "$markdown_text_log_file"
+
+eval "$basedir"/github-issue-list.sh
 
 cat "$markdown_text_log_file" | \
   TITLE="$ja_github_title" \
