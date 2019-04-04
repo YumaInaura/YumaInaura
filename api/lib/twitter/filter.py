a61612856d9ed733362d5f79788441c165b1ee00
Yumainaura <yuma.inaura@gmail.com>
2019-04-04 16:03:27 +0900

    no named commit

--- api/lib/twitter/filter.py
+++ api/lib/twitter/filter.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python3
 
-import sys, json, pbm
+import sys, json, re
 from optparse import OptionParser
 
 parser = OptionParser()
 
 parser.add_option("-e", "--end-with", dest="end_with")
+parser.add_option("-m", "--match", dest="match")
 (options, args) = parser.parse_args()
 
 end_with = options.end_with
 endswith = tuple(filter(None, end_with.split(',')))
+
+match_on = re.compile(options.match)
 
 tweets = json.loads(sys.stdin.read())
 
 results = []
 
 for tweet in tweets:
-  if end_with and not tweet['full_text'].endswith(endswith):
-    continue
+  hit = False
 
-  results.append(tweet)
+  if end_with and tweet['full_text'].endswith(endswith):
+    hit = True
+
+  if match_on and re.match(match_on, tweet['full_text']):
+    hit = True
+
+  if hit:
+    results.append(tweet)
 
 print(json.dumps(results))
