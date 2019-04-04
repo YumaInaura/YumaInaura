af8266cf641a0dae4a06c4e94793315846b2f22f
Yumainaura <yuma.inaura@gmail.com>
2019-04-04 15:29:44 +0900

    no named commit

--- api/lib/twitter/filter.py
+++ api/lib/twitter/filter.py
@@ -8,16 +8,16 @@ parser = OptionParser()
 parser.add_option("-e", "--end-with", dest="end_with")
 (options, args) = parser.parse_args()
 
-print(options.end_with)
-exit()
-
-find_hashtags = sys.argv
+end_with = options.end_with
 
 tweets = json.loads(sys.stdin.read())
 
 results = []
 
 for tweet in tweets:
+  if end_with and not tweet['full_text'].endswith(end_with):
+    continue
+
   results.append(tweet)
 
 print(json.dumps(results))
