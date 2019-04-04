08bc1b82d80bb451c7999ab162586055bd4760f3
Yumainaura <yuma.inaura@gmail.com>
2019-04-04 15:22:25 +0900

    no named commit

--- python/opt.py
+++ python/opt.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
-import sys, json, pbm
 from optparse import OptionParser
 
 parser = OptionParser()
 
-parser.add_option("-e", "--end-with", dest="end_with")
+parser.add_option("--some", dest="some_option")
 (options, args) = parser.parse_args()
 
-print(options.end_with)
-exit()
+print(options.some_option)
 
