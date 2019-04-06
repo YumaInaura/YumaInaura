53a549b47a77cdae533ebba7f5738cd3ba4098be
Yumainaura <yuma.inaura@gmail.com>
2019-04-06 15:56:28 +0900

    no named commit

--- api/lib/google-translate/translate-docker.sh
+++ api/lib/google-translate/translate-docker.sh
@@ -3,6 +3,6 @@
 basedir=$(dirname "$0")
 
 TOKEN=$(docker exec gcp /bin/bash ./get-token.sh) \
-  python "$basedir"/translate.py | \
+  eval "$basedir"/translate.py | \
   jq -r '.data.translations[].translatedText'
 
