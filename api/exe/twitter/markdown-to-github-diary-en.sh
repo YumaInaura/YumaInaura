4dbf6829a31e1602ed7395298a1450d080e7f365
Yumainaura <yuma.inaura@gmail.com>
2019-04-06 19:43:11 +0900

    no named commit

---
+++ api/exe/twitter/markdown-to-github-diary-en.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eu
+
+export LC_CTYPE=en_US.UTF-8
+
+basedir=$(dirname "$0")
+source "${basedir}/../../setting.sh"
+
+jst_date=$(TZ=Asia/Tokyo date --date='1 days ago' +'%Y-%m-%d')
+
+export OWNER=YumaInaura \
+       REPOSITORY=YumaInaura \
+       API_KEY="$github_api_key" \
+       TITLE="Yuma Inaura was Here ${jst_date} on Twitter" \
+       FILE="${log_dir}yumainaura2nd.md" \
+       LABELS=medium,english,twitter
+
+python "${api_dir}/github/create-or-edit-issue.py"
