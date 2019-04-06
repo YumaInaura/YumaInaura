500c73eb817f507cedf5fa856097ac27c10f12e2
Yumainaura <yuma.inaura@gmail.com>
2019-04-06 12:25:35 +0900

    no named commit

--- api/lib/google-translate/translate.sh
+++ api/lib/google-translate/translate.sh
@@ -2,6 +2,5 @@
 
 basedir=$(dirname "$0")
 
-GOOGLE_APPLICATION_CREDENTIALS=~/.secret/google-service-credential.json gcloud auth application-default print-access-token
-
 TOKEN=$("$basedir"/get-token.sh) python "$basedir"/translate.py
+
