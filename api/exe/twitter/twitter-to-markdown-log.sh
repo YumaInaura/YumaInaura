74d6e75939e93ff11f540f0f09253d87a8d9830f
Yumainaura <yuma.inaura@gmail.com>
2019-04-06 18:52:19 +0900

    no named commit

--- api/exe/twitter/twitter-to-markdown-log.sh
+++ api/exe/twitter/twitter-to-markdown-log.sh
@@ -3,32 +3,12 @@
 set -eu
 
 export LC_CTYPE=en_US.UTF-8
-
 basedir=$(dirname "$0")
 
 source "${basedir}/../../setting.sh"
-
 jst_date=$(TZ=Asia/Tokyo date --date='1 days ago' +'%y-%m-%d')
 
-mkdir -p "$log_dir"
-
-cp ~/.secret/twitter-yumainaura2nd-config.py "$api_dir"/twitter/config.py
-ALL=1 "$api_dir"/twitter/timeline.py > "$log_dir"/timeline-yumainaura-2nd.json
-
-cat "$log_dir"/timeline-yumainaura-2nd.json | OWN_USER_ID=1095662627890425900 "$api_dir"/twitter/filter-own.py > "$log_dir"/timeline-own-tweet-yumainaura2nd.json
-cat "$log_dir"/timeline-own-tweet-yumainaura2nd.json | "$api_dir"/twitter/jst-datetime-filter.py > "$log_dir"/timeline-jst-yesterday-yumainaura2nd.json
-cat "$log_dir"/timeline-jst-yesterday-yumainaura2nd.json | "$api_dir"/twitter/markdown.py > "$log_dir"/yumainaura2nd.md
-
-cp ~/.secret/twitter-yumainaura-config.py "$api_dir"/twitter/config.py
-ALL=1 "$api_dir"/twitter/timeline.py > "$log_dir"/timeline.json
-
-cat "$log_dir"/timeline.json | OWN_USER_ID=473780756 "$api_dir"/twitter/filter-own.py > "$log_dir"/timeline-own-tweet.json
-cat "$log_dir"/timeline-own-tweet.json | "$api_dir"/twitter/jst-datetime-filter.py > "$log_dir"/timeline-jst-yesterday.json
-cat "$log_dir"/timeline-jst-yesterday.json | "$api_dir"/twitter/markdown.py > "$log_dir"/yumainaura.md
-
-cat "$log_dir"/timeline-jst-yesterday.json | "$api_dir"/twitter/filter.py \
-  --end-with=j \
-  --match='エンジニ|プログラ|仕事|就職|Wanted|Qiita|python|ruby|vue|docker' \
-  | "$api_dir"/twitter/markdown.py \
-    > "$log_dir"/samurai.md
+eval "$basedir"/twitter-to-markdown-samurai.sh
+eval "$basedir"/twitter-to-markdown-yumainaura.sh
+eval "$basedir"/twitter-to-markdown-yumainaura2nd.sh
 
---
+++ api/exe/twitter/twitter-to-markdown-samurai.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -eu
+
+export LC_CTYPE=en_US.UTF-8
+basedir=$(dirname "$0")
+
+source "${basedir}/../../setting.sh"
+jst_date=$(TZ=Asia/Tokyo date --date='1 days ago' +'%y-%m-%d')
+
+mkdir -p "$log_dir"
+
+cat "$log_dir"/timeline-jst-yesterday.json | "$api_dir"/twitter/filter.py \
+  --end-with=j \
+  --match='エンジニ|プログラ|仕事|就職|Wanted|Qiita|python|ruby|vue|docker' \
+  | "$api_dir"/twitter/markdown.py \
+    > "$log_dir"/samurai.md
+
