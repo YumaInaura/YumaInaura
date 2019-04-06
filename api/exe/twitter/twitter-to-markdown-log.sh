490f7a2e4b60515ca63d8790f2949570ae5d5111
Yumainaura <yuma.inaura@gmail.com>
2019-04-06 17:25:57 +0900

    no named commit

--- api/exe/twitter/twitter-to-markdown-log.sh
+++ api/exe/twitter/twitter-to-markdown-log.sh
@@ -12,20 +12,23 @@ jst_date=$(TZ=Asia/Tokyo date --date='1 days ago' +'%y-%m-%d')
 
 api_dir="${basedir}/../../lib"
 
+log_dir="$basedir"/log
+mkdir -p "$log_dir"
+
 pushd ${api_dir}/twitter
   mkdir -p log
-  ALL=1 ./timeline.py > log/timeline.log
-  cat log/timeline.log | OWN_USER_ID=473780756 ./filter-own.py > log/timeline-own-tweet.log
-  cat log/timeline-own-tweet.log | ./jst-datetime-filter.py > log/timeline-jst-yesterday.log
+  ALL=1 ./timeline.py > "$log_dir"/timeline.log
+  cat "$log_dir"/timeline.log | OWN_USER_ID=473780756 ./filter-own.py > "$log_dir"/timeline-own-tweet.log
+  cat "$log_dir"/timeline-own-tweet.log | ./jst-datetime-filter.py > "$log_dir"/timeline-jst-yesterday.log
 
-  cat log/timeline-jst-yesterday.log | ./markdown.py > log/markdown.log
+  cat "$log_dir"/timeline-jst-yesterday.log | ./markdown.py > "$log_dir"/markdown.log
 
-  cat log/markdown.log | "$api_dir"/google-translate/translate.sh  > log/en-translated.md
+  cat "$log_dir"/markdown.log | "$api_dir"/google-translate/translate.sh  > "$log_dir"/en-translated.md
  
-   cat log/timeline-jst-yesterday.log | ./filter.py \
+   cat "$log_dir"/timeline-jst-yesterday.log | ./filter.py \
      --end-with=j \
      --match='エンジニ|プログラ|仕事|就職|Wanted|Qiita|python|ruby|vue|docker' \
      | ./markdown.py \
-       > log/samurai.md
+       > "$log_dir"/samurai.md
 popd
 
