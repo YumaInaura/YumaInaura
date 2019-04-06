37811f77d122afe42695295b063cd49da97489a9
Yumainaura <yuma.inaura@gmail.com>
2019-04-06 19:00:52 +0900

    no named commit

--- api/exe/twitter/twitter-to-markdown-yumainaura2nd.sh
+++ api/exe/twitter/twitter-to-markdown-yumainaura2nd.sh
@@ -15,5 +15,5 @@ ALL=1 "$api_dir"/twitter/timeline.py > "$log_dir"/timeline-yumainaura-2nd.json
 
 cat "$log_dir"/timeline-yumainaura-2nd.json | OWN_USER_ID=1095662627890425900 "$api_dir"/twitter/filter-own.py > "$log_dir"/timeline-own-tweet-yumainaura2nd.json
 cat "$log_dir"/timeline-own-tweet-yumainaura2nd.json | "$api_dir"/twitter/jst-datetime-filter.py > "$log_dir"/timeline-jst-yesterday-yumainaura2nd.json
-cat "$log_dir"/timeline-jst-yesterday-yumainaura2nd.json | "$api_dir"/twitter/markdown.py > "$log_dir"/yumainaura2nd.md
+cat "$log_dir"/timeline-jst-yesterday-yumainaura2nd.json | PERIOD='\\.' "$api_dir"/twitter/markdown.py > "$log_dir"/yumainaura2nd.md
 
