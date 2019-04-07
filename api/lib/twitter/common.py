a8cc4051f6df240aaf88edb3f6e3cce15049ec6f
Yumainaura <yuma.inaura@gmail.com>
2019-04-07 09:03:15 +0900

    no named commit

---
+++ api/lib/twitter/common.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import json, config, os, re
+from requests_oauthlib import OAuth1Session
+
+if os.environ.get('TWITTER_CONSUMER_KEY'):
+  CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
+  CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
+  ACCESS_TOKEN = os.environ.get('TWITTER_ACCESS_TOKEN')
+  ACCESS_TOKEN_SECRET = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+else:
+  CONSUMER_KEY = config.CONSUMER_KEY
+  CONSUMER_SECRET = config.CONSUMER_SECRET
+  ACCESS_TOKEN = config.ACCESS_TOKEN
+  ACCESS_TOKEN_SECRET = config.ACCESS_TOKEN_SECRET
+
+twitter = OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+params = json.loads()
+
+api_url = os.environ.get('API_URL')
+
+if os.environ.get('POST'):
+  response = twitter.post(api_url, params=params)
+else:
+  response = twitter.get(api_url, params=params)
+
+result = response.json()
+
+print(json.dumps(result))
