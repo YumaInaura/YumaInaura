b3e1e1b7d873d17d2fe5a79ee2e0b97f37aa9a13
Yumainaura <yuma.inaura@gmail.com>
2019-04-08 16:38:30 +0900

    no named commit

--- api/exe/twitter-tlanslate/ja-timeline.sh
+++ api/exe/twitter-tlanslate/ja-timeline.sh
@@ -9,13 +9,21 @@ mkdir -p "$log_dir"
 
 interval_second=${INTERVAL:-60}
 
+USER_ID="473780756"
+
 cp ~/.secret/twitter-yumainaura-config.py "$api_dir"/twitter/config.py
+
 ALL=1 ROUND=1 "$api_dir"/twitter/timeline.py > "$log_dir"/ja-timeline.json
 
+cat "$log_dir"/ja-timeline.json | OWN_USER_ID="$USER_ID" "$api_dir"/twitter/filter-own.py > "$log_dir"/ja-timeline-own.json
+
 start_unixtimestamp=$(($(date +%s)-$interval_second))
-cat "$log_dir"/ja-timeline.json | "$api_dir"/twitter/filter-timestamp.py "$start_unixtimestamp" | tee "$log_dir"/ja-timeline-recent.json
+cat "$log_dir"/ja-timeline-own.json | "$api_dir"/twitter/filter-timestamp.py "$start_unixtimestamp" | tee "$log_dir"/ja-timeline-recent.json
 
-cat "$log_dir"/ja-timeline-recent.json | jq '.[].full_text' | perl -pe 's/^"|"$//g' | tee "$log_dir"/ja-text.json
+cat "$log_dir"/ja-timeline-recent.json | jq '.[].full_text' | perl -pe 's/^"|"$//g' | tee "$log_dir"/ja-text.log
 
-
+rm "$log_dir"/en-text.log
+for ja_text in $(cat "$log_dir"/ja-text.log); do
+  "$api_dir"/google-translate/translate.sh >> "$log_dir"/en-text.log"
+done
 
