83e5064ce7f380b16075405ae3570762c00c908b
Yumainaura <yuma.inaura@gmail.com>
2019-04-08 15:57:26 +0900

    no named commit

--- api/exe/twitter-tlanslate/ja-to-en.sh
+++ api/exe/twitter-tlanslate/ja-to-en.sh
@@ -2,13 +2,13 @@
 
 set -eu
 
-export LC_CTYPE=en_US.UTF-8
 basedir=$(dirname "$0")
 source "${basedir}/../../setting.sh"
 
 mkdir -p "$log_dir"
 
 cp ~/.secret/twitter-yumainaura-config.py "$api_dir"/twitter/config.py
-ALL=1 "$api_dir"/twitter/timeline.py > "$log_dir"/timeline.json
+ALL=1 ROUND=1 "$api_dir"/twitter/timeline.py > "$log_dir"/timeline.json
 
 cp ~/.secret/twitter-yumainaura2nd-config.py "$api_dir"/twitter/config.py
+
