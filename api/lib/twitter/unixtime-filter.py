d309095c0fa2b2c3f2d5c5d42da1d61018ac1e7e
Yumainaura <yuma.inaura@gmail.com>
2019-04-08 13:02:53 +0900

    no named commit

--- api/lib/twitter/unixtime-filter.py
+++ api/lib/twitter/unixtime-filter.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 
-import json, config, os, re, sys, time, datetime
+import json, os, sys, time, datetime
 from datetime import timedelta
-from optparse import OptionParser
-
-timelines = json.loads(sys.stdin.read())
-
-parser = OptionParser()
 
 start_unixtimestamp = int(sys.argv[1])
 end_unixtimestamp = int(sys.argv[2]) if 2 in sys.argv else sys.argv[2]
@@ -16,6 +11,8 @@ def convert_to_datetime(datetime_str):
 
   tweet_datetime = datetime.datetime(*tweet_time[:6])
   return(tweet_datetime)
+
+timelines = json.loads(sys.stdin.read())
 
 for tweet in timelines:
   tweet_datetime = convert_to_datetime(tweet['created_at'])
