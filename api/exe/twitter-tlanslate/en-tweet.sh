1c23ab569c96312ef318a117f704d63ede08ccc9
Yumainaura <yuma.inaura@gmail.com>
2019-04-08 17:39:07 +0900

    no named commit

--- api/exe/twitter-tlanslate/en-tweet.sh
+++ api/exe/twitter-tlanslate/en-tweet.sh
@@ -7,7 +7,7 @@ source "${basedir}/../../setting.sh"
 
 cp ~/.secret/twitter-yumainaura2nd-config.py "$api_dir"/twitter/config.py
 
-for en_text in "$(cat "$log_dir"/en-text.log)"; do
+for en_text in "$(cat "$log_dir"/en-text-trancate.log)"; do
   echo "$en_text" | "$api_dir"/twitter/create.py
 done
 
