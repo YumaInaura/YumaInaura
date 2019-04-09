b6b5c4800ea13111b8457f78afaaf76e98691a89
Yumainaura <yuma.inaura@gmail.com>
2019-04-10 08:25:00 +0900

    no named commit

--- api/lib/twitter/upstream-tweet-chain-by-timeline.py
+++ api/lib/twitter/upstream-tweet-chain-by-timeline.py
@@ -2,21 +2,20 @@
 
 # https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline.html
 
-import json, config, os, re, twitterauth
+import json, os, re, twitterauth, sys
 
 twitter = twitterauth.twitter()
 
 api_url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
-last_id = ''
-
-last_tweet = {}
-tweets = []
 
 MAX_ROUND = int(os.environ.get('ROUND')) if os.environ.get('ROUND') else 30
 
 tweet_url = sys.argv[1]
 
-tweet_id = search(r'/status/(?P<id>\d+)', tweet_url)['id']
+tweet_id = re.search(r'/status/(?P<id>\d+)', tweet_url)['id']
+
+last_tweet = {}
+tweets = []
 
 for i in range(1, MAX_ROUND+1):
   status_id = last_tweet['in_reply_to_status_id_str'] if last_tweet and last_tweet['in_reply_to_status_id_str'] else tweet_id
--- api/lib/twitter/upstream-tweet-to-markdown.sh
+++ api/lib/twitter/upstream-tweet-to-markdown.sh
@@ -4,4 +4,8 @@ set -eu
 
 basedir=$(dirname "$0")
 
+tweet_url="$1"
+eval "$basedir"/upstream-tweet-chain-by-timeline.py \
+  | "$tweet_url" \
+  > "$basedir"/log/upstream-tweet-chain-by-timeline.json"
 
