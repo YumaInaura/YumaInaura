cbc6c44f60737f1f1c49ee77f86723312676af0f
Yumainaura <yuma.inaura@gmail.com>
2019-04-09 12:20:20 +0900

    no named commit

--- api/exe/twitter-tlanslate/en-translate.sh
+++ api/exe/twitter-tlanslate/en-translate.sh
@@ -7,9 +7,13 @@ source "${basedir}/../../setting.sh"
 
 rm -f "$log_dir"/en-text.log
 
-for ja_text in "$(cat "$log_dir"/ja-text.log)"; do
-  echo "$ja_text" | "$api_dir"/google-translate/translate.sh >> "$log_dir"/en-text.log
+i=1
+for ja_text in echo $(cat "$log_dir"/ja-text.log); do
+  # echo "$ja_text" | "$api_dir"/google-translate/translate.sh >> "$log_dir"/en-text_"$i".log
+  echo "$ja_text"
+  echo $i
+  i=$((i+i))
 done
 
-cat "$log_dir"/en-text.log
+#cat "$log_dir"/en-text.log
 
