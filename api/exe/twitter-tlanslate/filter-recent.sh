aad70fd7840bf19b24d08bb8b29ed80bdf63ea14
Yumainaura <yuma.inaura@gmail.com>
2019-04-09 11:50:34 +0900

    no named commit

---
+++ api/exe/twitter-tlanslate/filter-recent.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eu
+
+basedir=$(dirname "$0")
+source "${basedir}/../../setting.sh"
+
+USER_ID="473780756"
+cat "$log_dir"/ja-timeline.json | OWN_USER_ID="$USER_ID" "$api_dir"/twitter/filter-own.py > "$log_dir"/ja-timeline-own.json
+
+interval_second=${INTERVAL:-60}
+start_unixtimestamp=$(($(date +%s)-$interval_second))
+
+cat "$log_dir"/ja-timeline-own.json | "$api_dir"/twitter/filter-timestamp.py "$start_unixtimestamp" | tee "$log_dir"/ja-timeline-recent.json
+
--- api/exe/twitter-tlanslate/ja-text.sh
+++ api/exe/twitter-tlanslate/ja-text.sh
@@ -5,14 +5,6 @@ set -eu
 basedir=$(dirname "$0")
 source "${basedir}/../../setting.sh"
 
-USER_ID="473780756"
-cat "$log_dir"/ja-timeline.json | OWN_USER_ID="$USER_ID" "$api_dir"/twitter/filter-own.py > "$log_dir"/ja-timeline-own.json
-
-interval_second=${INTERVAL:-60}
-start_unixtimestamp=$(($(date +%s)-$interval_second))
-
-cat "$log_dir"/ja-timeline-own.json | "$api_dir"/twitter/filter-timestamp.py "$start_unixtimestamp" | tee "$log_dir"/ja-timeline-recent.json
-
 cat "$log_dir"/ja-timeline-recent.json | \
   jq '.[] | "https://twitter.com/YumaInaura/status/" +  .id_str + " " + .full_text' | \
   tee "$log_dir"/ja-text.log
