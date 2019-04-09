02b4909fb314721f8cd723f9986e90c10b9f52c1
Yumainaura <yuma.inaura@gmail.com>
2019-04-09 19:18:46 +0900

    no named commit

--- api/exe/twitter-tlanslate/en-tweet.sh
+++ api/exe/twitter-tlanslate/en-tweet.sh
@@ -6,7 +6,7 @@ basedir=$(dirname "$0")
 source "${basedir}/../../setting.sh"
 
 cat "$log_dir"/en-translated.json | \
-  jq ' "https://twitter.com/YumaInaura/status/" +  .[].id_str + " " + .[].trunslated_text' | \
+  jq ' "https://twitter.com/YumaInaura/status/" +  .[].id_str + " " + .[].translated_text' | \
   jq '[{ "text" : . }]' | \
   tee "$log_dir"/en-tweet.json
 
