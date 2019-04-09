6799961628eb1acdd6fc6dca74af86a96502e9d8
Yumainaura <yuma.inaura@gmail.com>
2019-04-10 08:09:41 +0900

    no named commit

--- api/lib/twitter/upstream-tweet-chain-by-timeline.py
+++ api/lib/twitter/upstream-tweet-chain-by-timeline.py
@@ -2,24 +2,9 @@
 
 # https://developer.twitter.com/en/docs/tweets/timelines/api-reference/get-statuses-user_timeline.html
 
-import json, config, os, re
-from requests_oauthlib import OAuth1Session
-import time
-import datetime
-from datetime import timedelta
+import json, config, os, re, twitterauth
 
-if os.environ.get('TWITTER_CONSUMER_KEY'):
-  CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
-  CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
-  ACCESS_TOKEN = os.environ.get('TWITTER_ACCESS_TOKEN')
-  ACCESS_TOKEN_SECRET = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
-else:
-  CONSUMER_KEY = config.CONSUMER_KEY
-  CONSUMER_SECRET = config.CONSUMER_SECRET
-  ACCESS_TOKEN = config.ACCESS_TOKEN
-  ACCESS_TOKEN_SECRET = config.ACCESS_TOKEN_SECRET
-
-twitter = OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+twitter = twitterauth.twitter()
 
 api_url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
 last_id = ''
@@ -29,8 +14,12 @@ tweets = []
 
 MAX_ROUND = int(os.environ.get('ROUND')) if os.environ.get('ROUND') else 30
 
+tweet_url = sys.argv[1]
+
+tweet_id = search(r'/status/(?P<id>\d+)', tweet_url)['id']
+
 for i in range(1, MAX_ROUND+1):
-  status_id = last_tweet['in_reply_to_status_id_str'] if last_tweet and last_tweet['in_reply_to_status_id_str'] else os.environ.get('ID')
+  status_id = last_tweet['in_reply_to_status_id_str'] if last_tweet and last_tweet['in_reply_to_status_id_str'] else tweet_id
 
   api_params = {
     'trim_user' : True,
