7993f763f13ed4f4a71ac1159241976174508d23
Yumainaura <yuma.inaura@gmail.com>
2019-04-09 12:01:42 +0900

    no named commit

--- api/exe/twitter-tlanslate/en-translate.sh
+++ api/exe/twitter-tlanslate/en-translate.sh
@@ -13,12 +13,3 @@ done
 
 cat "$log_dir"/en-text.log
 
-
-rm -f "$log_dir"/en-text-trancate.log
-
-for en_text in "$(cat "$log_dir"/en-text.log)"; do
-  echo "$en_text" | perl -pe 's/^(.{280}).+/\1/g' >> "$log_dir"/en-text-trancate.log
-done
-
-cat "$log_dir"/en-text-trancate.log
-
