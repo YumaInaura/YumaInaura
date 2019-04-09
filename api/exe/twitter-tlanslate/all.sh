9f46ab7b220e0cfe42cd1cb37737ae7a1d365bdd
Yumainaura <yuma.inaura@gmail.com>
2019-04-09 19:01:53 +0900

    no named commit

--- api/exe/twitter-tlanslate/all.sh
+++ api/exe/twitter-tlanslate/all.sh
@@ -5,7 +5,8 @@ set -eu
 basedir=$(dirname "$0")
 source "${basedir}/../../setting.sh"
 
-"$basedir"/en-translate.sh*
-"$basedir"/en-tweet.sh*
-"$basedir"/ja-filter.sh*
-"$basedir"/ja-timeline.sh*
+"$basedir"/ja-timeline.sh
+"$basedir"/filter-recent.sh
+"$basedir"/en-translate.sh
+"$basedir"/en-tweet.sh
+
