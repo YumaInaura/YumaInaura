73d796a63b11829e0c8370a87aa023d5a4aad540
Yumainaura <yuma.inaura@gmail.com>
2019-04-10 12:59:13 +0900

    no named commit

--- api/exe/twitter-translate/all.sh
+++ api/exe/twitter-translate/all.sh
@@ -8,5 +8,6 @@ source "${basedir}/../../setting.sh"
 eval "$basedir"/ja-timeline.sh
 eval "$basedir"/filter-recent.sh
 eval "$basedir"/en-translate.sh
+eval "$basedir"/en-seed.sh
 eval "$basedir"/en-tweet.sh
 
