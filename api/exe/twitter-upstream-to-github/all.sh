356775dc7f763e7efd80707e1f1b9d4eb751ec1f
Yumainaura <yuma.inaura@gmail.com>
2019-04-10 10:36:06 +0900

    no named commit

--- api/exe/twitter-upstream-to-github/all.sh
+++ api/exe/twitter-upstream-to-github/all.sh
@@ -5,6 +5,7 @@ set -eu
 base_dir=$(dirname "$0")
 source "${base_dir}/../../setting.sh"
 
-eval "$base_dir"/markdown.sh*
-eval "$base_dir"/upstream-timeline.sh
+eval "$base_dir"/upstream-timeline.sh "$1"
+eval "$base_dir"/markdown.sh
 eval "$base_dir"/github-create-issue.sh
+
