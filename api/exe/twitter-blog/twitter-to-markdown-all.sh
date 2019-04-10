3f04a4789ce0da989869463b8c092c04eeb3f962
Yumainaura <yuma.inaura@gmail.com>
2019-04-10 14:57:43 +0900

    no named commit

--- api/exe/twitter-blog/twitter-to-markdown-all.sh
+++ api/exe/twitter-blog/twitter-to-markdown-all.sh
@@ -8,7 +8,7 @@ basedir=$(dirname "$0")
 source "${basedir}/../../setting.sh"
 jst_date=$(TZ=Asia/Tokyo date --date='1 days ago' +'%y-%m-%d')
 
-eval "$basedir"/twitter-to-markdown-samurai.sh
 eval "$basedir"/twitter-to-markdown-yumainaura.sh
+eval "$basedir"/twitter-to-markdown-samurai.sh
 eval "$basedir"/twitter-to-markdown-yumainaura2nd.sh
 
