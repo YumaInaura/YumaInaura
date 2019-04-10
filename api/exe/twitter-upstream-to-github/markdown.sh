6c71e16442c48d47a837bb9d9092c89078b2c6bc
Yumainaura <yuma.inaura@gmail.com>
2019-04-10 09:24:16 +0900

    no named commit

--- api/exe/twitter-upstream-to-github/markdown.sh
+++ api/exe/twitter-upstream-to-github/markdown.sh
@@ -3,9 +3,11 @@
 set -eu
 
 base_dir=$(dirname "$0")
+source "${base_dir}/../../setting.sh"
+
 log_dir="$base_dir"/log
 
 cat "$log_dir"/upstream-tweet-chain-by-timeline.json \
-  | "$base_dir"/../markdown.py \
-  > "$log_dir"/markdown.txt
+  | "$api_dir"/twitter/markdown.py \
+  > "$log_dir"/github-body.md
 
--- api/exe/twitter-upstream-to-github/upstream-timeline.sh
+++ api/exe/twitter-upstream-to-github/upstream-timeline.sh
@@ -3,12 +3,13 @@
 set -eu
 
 base_dir=$(dirname "$0")
-log_dir="$base_dir"/log
+source "${base_dir}/../../setting.sh"
 
+log_dir="$base_dir"/log
 mkdir -p "$log_dir"
 
 tweet_url="$1"
 
-eval "$base_dir"/../upstream-tweet-chain-by-timeline.py "$tweet_url" \
+eval "$api_dir"/twitter/upstream-tweet-chain-by-timeline.py "$tweet_url" \
   > "$log_dir"/upstream-tweet-chain-by-timeline.json
 
