7c02fea5fc2739080e7ad94a1344a3c64883a973
Yumainaura <yuma.inaura@gmail.com>
2019-04-10 10:55:29 +0900

    no named commit

---
+++ api/exe/twitter-translate/en-tweet-seed.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -eu
+
+basedir=$(dirname "$0")
+source "${basedir}/../../setting.sh"
+
+cat "$log_dir"/en-translated.json | \
+  jq ' "https://twitter.com/YumaInaura/status/" +  .[].id_str + " " + .[].translated_text' | \
+  jq '[{ "text" : . }]' | \
+  tee "$log_dir"/en-tweet.json
+
--- api/exe/twitter-translate/en-tweet.sh
+++ api/exe/twitter-translate/en-tweet.sh
@@ -5,11 +5,6 @@ set -eu
 basedir=$(dirname "$0")
 source "${basedir}/../../setting.sh"
 
-cat "$log_dir"/en-translated.json | \
-  jq ' "https://twitter.com/YumaInaura/status/" +  .[].id_str + " " + .[].translated_text' | \
-  jq '[{ "text" : . }]' | \
-  tee "$log_dir"/en-tweet.json
-
 cp ~/.secret/twitter-yumainaura2nd-config.py "$api_dir"/twitter/config.py
 
 cat "$log_dir"/en-tweet.json | \
