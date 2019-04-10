2c2af72a06e599532a8a74d61410f07aa16f4986
Yumainaura <yuma.inaura@gmail.com>
2019-04-10 09:18:20 +0900

    no named commit

--- api/lib/twitter/upstream-tweet-to-markdown/markdown.sh
+++ api/lib/twitter/upstream-tweet-to-markdown/markdown.sh
@@ -6,6 +6,6 @@ base_dir=$(dirname "$0")
 log_dir="$base_dir"/log
 
 cat "$log_dir"/upstream-tweet-chain-by-timeline.json \
-  "$base_dir"/../markdown.py \
+  | "$base_dir"/../markdown.py \
   > "$log_dir"/markdown.txt
 
