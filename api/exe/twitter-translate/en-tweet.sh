8a9faf57873b0fb5396a857bd8f5bd566619945a
Yumainaura <yuma.inaura@gmail.com>
2019-04-10 11:42:07 +0900

    no named commit

--- api/exe/twitter-translate/en-tweet.sh
+++ api/exe/twitter-translate/en-tweet.sh
@@ -7,7 +7,7 @@ source "${basedir}/../../setting.sh"
 
 cp ~/.secret/twitter-yumainaura2nd-config.py "$api_dir"/twitter/config.py
 
-cat "$log_dir"/en-tweet-seed.json | \
+cat "$log_dir"/en-translated.json | \
   JSON_KEY="translated_text" \
   MAX_LENGTH=280 \
     "$api_dir"/twitter/create.py | \
