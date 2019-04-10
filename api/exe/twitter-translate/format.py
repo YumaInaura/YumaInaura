df724c5785657f8b074cf521b790e856a5bc9bbe
Yumainaura <yuma.inaura@gmail.com>
2019-04-10 12:50:16 +0900

    no named commit

---
+++ api/exe/twitter-translate/format.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys, json, re, os
+
+tweets = json.loads(sys.stdin.read())
+
+results = []
+
+for tweet in tweets:
+  tweet['text'] = tweet['trunslated_text'][:250] + tweet['url']
+  results.append(tweet)
+
+print(json.dumps(results))
+
