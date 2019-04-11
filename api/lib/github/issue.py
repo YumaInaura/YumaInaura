f5898d910123b47fc68247e67a715fd234c10956
Yumainaura <yuma.inaura@gmail.com>
2019-04-12 07:26:05 +0900

    no named commit

--- api/lib/github/issue.py
+++ api/lib/github/issue.py
@@ -8,7 +8,7 @@ repository = os.environ.get('REPOSITORY')
 
 results = []
 
-round = int(os.environ.get('ROUND')) if os.environ.get('ROUND') else 1
+round = int(os.environ.get('ROUND')) if os.environ.get('ROUND') else 3
 
 for i in range(0, round):
   api_url = 'https://api.github.com/repos/' + owner + '/' + repository + '/issues?page=' + str(i)
