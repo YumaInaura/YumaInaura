237aceb04247362795dc53306479c95cd70be71c
Yumainaura <yuma.inaura@gmail.com>
2019-04-12 07:15:23 +0900

    no named commit

---
+++ api/exe/twitter-translate/zh-CN-translate.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -eu
+
+basedir=$(dirname "$0")
+source "${basedir}/../../setting.sh"
+
+if [ $(cat "$log_dir"/ja-timeline-recent.json | jq '. | length') -eq 0 ]; then
+  echo "No translate resource"
+  exit 1
+fi
+
+cat "$log_dir"/ja-timeline-recent.json | \
+  TRANSLATE_JSON_KEY=full_text \
+  FROM=ja TO=zh-CN \
+    "$api_dir"/google-translate/translate-json.sh | \
+  tee "$log_dir"/en-translated.json 
+
