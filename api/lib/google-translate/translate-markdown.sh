2026632607c301dd49096c7933ce50c15fc5bb08
Yumainaura <yuma.inaura@gmail.com>
2019-04-12 08:51:54 +0900

    no named commit

--- api/lib/google-translate/translate-markdown.sh
+++ api/lib/google-translate/translate-markdown.sh
@@ -2,9 +2,21 @@
 
 base_dir=$(dirname "$0")
 
+log_dir="$base_dir"/log
+ 
+mkdir -p "$log_dir"
+
 cat /dev/stdin \
   | redcarpet --parse=fenced_code_blocks \
+  tee "$log_dir"/seed.html
+
+cat "$log_dir"/seed.html \
   | FORMAT=html ./translate.sh \
+  | tee "$log_dir"/translated.html
+
+cat "$log_dir"/translated.html \
   | jq . --raw-output \
-  | reverse_markdown
+  | reverse_markdown \
+  | tee "$log_dir"/translated.md
+
 
