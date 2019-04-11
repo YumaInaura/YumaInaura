9586c5733d1156553ce8cceca39649d8d2156ff6
Yumainaura <yuma.inaura@gmail.com>
2019-04-11 19:35:31 +0900

    no named commit

--- api/exe/twitter-translate/ja-en-tweet.sh
+++ api/exe/twitter-translate/ja-en-tweet.sh
@@ -7,6 +7,11 @@ source "${basedir}/../../setting.sh"
 
 source ~/.secret/env/twitter-yumainaura
 
+if [ $((RANDOM)) -gt 3000 ]; then
+  echo "no conitinue ja-en tweet  by random"
+  exit
+fi
+
 cat "$log_dir"/ja-en-seed.json | \
   JSON_KEY="text" \
   "$api_dir"/twitter/create.py | \
