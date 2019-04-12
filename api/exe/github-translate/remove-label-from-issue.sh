07758827143cb795e9fb7dc344976f7d71f302f7
Yumainaura <yuma.inaura@gmail.com>
2019-04-12 18:04:35 +0900

    no named commit

--- api/exe/github-translate/remove-label-from-issue.sh
+++ api/exe/github-translate/remove-label-from-issue.sh
@@ -6,6 +6,9 @@ base_dir=$(dirname "$0")
 source "${base_dir}/../../setting.sh"
 
 cat "$log_dir"/remove-label-from-issue-seed.json \
- | "$api_dir"/github/edit-issue.py \
+ | \
+    USER_NAME=YumaInaura \
+    API_KEY=$(cat ~/.secret/github-api-key) \
+      "$api_dir"/github/edit-issue.py \
  | tee "$log_dir"/removed-label-from-issue.json
 
