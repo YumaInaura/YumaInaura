1261234ecdb10ff43ff3750ece8ff314ed952e76
Yumainaura <yuma.inaura@gmail.com>
2019-04-12 13:25:17 +0900

    no named commit

--- api/exe/github-translate/en-format.sh
+++ api/exe/github-translate/en-format.sh
@@ -6,7 +6,7 @@ base_dir=$(dirname "$0")
 source "${base_dir}/../../setting.sh"
 
 cat "$log_dir"/en-need-translate-issue.json \
-  | "$base_dir"/format.py \
-  | tee "$log_dir"/en-format.json
+  | "$base_dir"/en-format.py \
+  | tee "$log_dir"/en-formatted.json
 
 
--- api/exe/github-translate/en-translate.sh
+++ api/exe/github-translate/en-translate.sh
@@ -5,8 +5,12 @@ set -eu
 base_dir=$(dirname "$0")
 source "${base_dir}/../../setting.sh"
 
-cat "$log_dir"/en-need-translate-issue.json \
-  | "$base_dir"/en-translate.py \
+cat "$log_dir"/en-formatted-html.json \
+  | \
+    FORMAT=html \
+    TOKEN=$("$api_dir/google-translate/get-token.sh") \
+    TRANSLATE_JSON_KEY=formatted_html
+      "$api_dir"/google-translate/translate-json.py \
   | tee "$log_dir"/en-translated.json
 
 
