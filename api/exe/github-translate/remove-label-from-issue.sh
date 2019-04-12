fb65a312dcb8bec09702787e145afa22cdeb7c96
Yumainaura <yuma.inaura@gmail.com>
2019-04-12 17:57:43 +0900

    no named commit

--- api/exe/github-translate/remove-label-from-issue.sh
+++ api/exe/github-translate/remove-label-from-issue.sh
@@ -6,12 +6,6 @@ base_dir=$(dirname "$0")
 source "${base_dir}/../../setting.sh"
 
 cat "$log_dir"/en-need-translate-issue.json \
- | \
-    FORMAT=text \
-    TOKEN=$("$api_dir"/google-translate/get-token.sh) \
-    TRANSLATE_JSON_KEY=title \
-    TRANSLATED_JSON_KEY=en_translated_title \
-      "$api_dir"/google-translate/translate-json.py \
-  | tee "$log_dir"/en-translated.json
-
+ | "$base_dir"/remove-label-from-issue-seed.py \
+ | tee "$log_dir"/remove-label-from-issue-seed.json
 
