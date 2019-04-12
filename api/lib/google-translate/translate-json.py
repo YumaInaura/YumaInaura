e7a6ec4e5c5165f87e58acf81a154ef0236bf3d0
Yumainaura <yuma.inaura@gmail.com>
2019-04-12 12:45:43 +0900

    no named commit

--- api/lib/google-translate/translate-json.py
+++ api/lib/google-translate/translate-json.py
@@ -1,35 +1,35 @@
 #!/usr/bin/env python3
-
 # -*- coding: utf-8 -*-
 
 import os, sys, requests, json, fileinput, re
 
-#resource_message = ''
-#for text in sys.stdin.readlines():
-#  resource_message += re.sub(r'\\n', "\n", text)
-
 resource_message = sys.stdin.read()
 
-from_language = os.environ.get('FROM') if os.environ.get('FROM') else 'ja'
-to_language = os.environ.get('TO') if os.environ.get('TO') else 'en'
-translate_format = os.environ.get('FORMAT') if os.environ.get('FORMAT') else 'text'
-
-data = {
-  'q': resource_message,
-  'source': from_language,
-  'target': to_language,
-  'format': translate_format
-}
-
-url = 'https://translation.googleapis.com/language/translate/v2'
 token = os.environ['TOKEN']
 
-headers = {
- 'Authorization': 'Bearer {}'.format(token),
- 'Content-Type': 'application/json',
-}
+results = []
 
-res = requests.post(url, headers=headers, json=data)
+for seed in json.dumps(sys.stdin.read()):
+  resouce_message = seed['text']
+  translate_format = os.environ.get('FORMAT') if os.environ.get('FORMAT') else seed['format']
+  from_language = os.environ.get('FROM') if os.environ.get('FROM') else seed['from']
+  to_language = os.environ.get('TO') if os.environ.get('TO') else seed['to']
 
-print(json.dumps(res.json()))
+  params = {
+    'q': resource_message,
+    'source': from_language,
+    'target': to_language,
+    'format': translate_format
+  }
+ 
+  api_url = 'https://translation.googleapis.com/language/translate/v2'
+ 
+  headers = {
+   'Authorization': 'Bearer {}'.format(token),
+   'Content-Type': 'application/json',
+  }
+ 
+  res = requests.post(api_url, headers=headers, json=params)
+ 
+print(json.dumps(results))
   
