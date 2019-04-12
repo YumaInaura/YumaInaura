e9957b82c91ebc78abb28b0795e1bcd183d935f3
Yumainaura <yuma.inaura@gmail.com>
2019-04-12 17:55:05 +0900

    no named commit

---
+++ api/exe/github-translate/remove-label-from-issue-seed.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys, json, re, os, subprocess
+
+issues = json.loads(sys.stdin.read())
+
+results = []
+
+for issue in issues:
+  result = {}
+
+  result['number']         = issue['number']
+  result['remove_labels']  = ['en-translate']
+  result['owner']          = 'YumaInaura'
+  result['repository']     = 'YumaInaura'
+
+  results.append(result)
+
+rint(json.dumps(results))
+
--- api/exe/github-translate/remove-label-from-issue.sh
+++ api/exe/github-translate/remove-label-from-issue.sh
@@ -5,14 +5,8 @@ set -eu
 base_dir=$(dirname "$0")
 source "${base_dir}/../../setting.sh"
 
-cat "$log_dir"/en-format.json \
-  | \
-    FORMAT=html \
-    TOKEN=$("$api_dir"/google-translate/get-token.sh) \
-    TRANSLATE_JSON_KEY=formatted_html \
-    TRANSLATED_JSON_KEY=en_translated_html \
-      "$api_dir"/google-translate/translate-json.py \
-  | \
+cat "$log_dir"/en-need-translate-issue.json \
+ | \
     FORMAT=text \
     TOKEN=$("$api_dir"/google-translate/get-token.sh) \
     TRANSLATE_JSON_KEY=title \
