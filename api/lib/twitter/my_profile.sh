fe47d9d8b9bde3dd8f0a32e52ee2ed85bd95a36e
Yumainaura <yuma.inaura@gmail.com>
2019-04-13 07:43:42 +0900

    no named commit

--- api/lib/twitter/my_profile.sh
+++ api/lib/twitter/my_profile.sh
@@ -2,6 +2,5 @@
 
 base_dir=$(dirname "$0")
 
-eval "$base_dir"/common.py "https://api.twitter.com/1.1/users/show.json" "{\"screen_name\": \"$1\"}"
-
+"$base_dir"/common.py  https://api.twitter.com/1.1/users/show.json '{"screen_name": "YumaInaura"}'
 
