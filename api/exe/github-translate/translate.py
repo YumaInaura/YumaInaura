d1b923a6f8e454cbc057b04881ebf0ff2191ac52
Yumainaura <yuma.inaura@gmail.com>
2019-04-12 11:01:17 +0900

    no named commit

--- api/exe/github-translate/translate.py
+++ api/exe/github-translate/translate.py
@@ -2,14 +2,23 @@
 
 import sys, json, re, os, subprocess
 
-tweets = json.loads(sys.stdin.read())
+issues = json.loads(sys.stdin.read())
 
 results = []
 
 for issue in issues:
   result = {}
-  result['html_body'] = subprocess.run(['echo', issue['body'] ,' | redcarpet'])
-  results.append(seed)
+
+  result['html'] = subprocess.run(['redcarpet'], stdout=subprocess.PIPE, input=issue['body'], encoding='utf-8').stdout
+
+  result['formatted_html'] = re.sub('\n', '<br>', result['html'])
+
+  result['translated_html_seed'] = subprocess.run(['../../lib/google-translate/translate-html.sh'], \
+    stdout=subprocess.PIPE, input=result['formatted_html'],  encoding='utf-8').stdout
+
+  result['translated_html'] = re.sub('<br>', '\n', result['translated_html_seed'])
+
+  results.append(result)
 
 print(json.dumps(results))
 
--- api/lib/google-translate/translate-html.sh
+++ api/lib/google-translate/translate-html.sh
@@ -3,6 +3,7 @@
 base_dir=$(dirname "$0")
 
 TOKEN=$("$base_dir"/get-token.sh) \
+  FORMAT=html \
   "$base_dir"/translate.py | \
-  jq '.data.translations[].translatedText'
+  jq -r '.data.translations[].translatedText'
 
