e41fdc8bbf18d4e048eafcf640cb0f960227db2b
Yumainaura <yuma.inaura@gmail.com>
2019-04-13 08:27:36 +0900

    no named commit

--- api/exe/twitter-blog/twitter-user-profile-to-markdown.sh
+++ api/exe/twitter-blog/twitter-user-profile-to-markdown.sh
@@ -10,11 +10,11 @@ source "${base_dir}/../twitter-setting.sh"
 "$api_dir"/twitter/user-show.sh "$TWITTER_JA_USER_NAME" \
   | jq '[.]' \
   | "$api_dir"/twitter/user-show-markdown.py \
-  | tee "$log_dir"/yumainaura.md
+  | tee "$log_dir"/"$TWITTER_JA_USER_NAME"-profile.md
 
 "$api_dir"/twitter/user-show.sh "$TWITTER_EN_USER_NAME" \
   | jq '[.]' \
   | "$api_dir"/twitter/user-show-markdown.py \
-  | tee "$log_dir"/yumainaura2nd.md
+  | tee "$log_dir"/"$TWITTER_EN_USER_NAME"-profile.md
 
 
