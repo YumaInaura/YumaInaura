5e468ce1f79de616f64f6f0138df31e59e137dfe
Yumainaura <yuma.inaura@gmail.com>
2019-04-12 15:13:32 +0900

    no named commit

--- api/lib/github/edit-issue.py
+++ api/lib/github/edit-issue.py
@@ -6,18 +6,18 @@ import os, json, re, requests, sys
 USER_NAME  = os.environ.get('USER_NAME')
 API_KEY = os.environ.get('API_KEY')
 
-edits = sys.stdin.read()
+edits = json.loads(sys.stdin.read())
 
 session = requests.Session()
 session.auth = (USER_NAME, API_KEY)
- 
+
 def get_issue(issue):
-  issue_api_url = 'https://api.github.com/repos/' + issue['owner'] + '/' + issue['repository'] + '/issues/' + issue['number']
+  issue_api_url = 'https://api.github.com/repos/' + issue['owner'] + '/' + issue['repository'] + '/issues/' + str(issue['number'])
   res = requests.get(issue_api_url)
   return(res.json())
 
 for edit in edits:
-  issue_number = edit['number']
+  issue_number = str(edit['number'])
   owner = edit['owner']
   repository = edit['repository']
 
@@ -25,7 +25,11 @@ for edit in edits:
 
   edit_api_url = 'https://api.github.com/repos/%s/%s/issues/%s' % (owner, repository, issue_number)
 
-  update = issue
+  update = {
+    "title", issue['title'],
+    "body", issue['body'],
+    "labels", issue['labels']
+  }
 
   res = session.post(edit_api_url, json.dumps(update))
 
