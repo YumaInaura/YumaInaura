a096863fd09f7c2544b2a4d6922c9a3ccab15e34
Yumainaura <yuma.inaura@gmail.com>
2019-04-12 12:41:36 +0900

    no named commit

--- api/lib/google-translate/translate-json.py
+++ api/lib/google-translate/translate-json.py
@@ -1,40 +1,35 @@
 #!/usr/bin/env python3
+
 # -*- coding: utf-8 -*-
 
 import os, sys, requests, json, fileinput, re
 
-json_lines = sys.stdin.read()
+#resource_message = ''
+#for text in sys.stdin.readlines():
+#  resource_message += re.sub(r'\\n', "\n", text)
+
+resource_message = sys.stdin.read()
 
 from_language = os.environ.get('FROM') if os.environ.get('FROM') else 'ja'
 to_language = os.environ.get('TO') if os.environ.get('TO') else 'en'
+translate_format = os.environ.get('FORMAT') if os.environ.get('FORMAT') else 'text'
 
+data = {
+  'q': resource_message,
+  'source': from_language,
+  'target': to_language,
+  'format': translate_format
+}
+
+url = 'https://translation.googleapis.com/language/translate/v2'
 token = os.environ['TOKEN']
-translate_json_key = os.environ['TRANSLATE_JSON_KEY'] if os.environ['TRANSLATE_JSON_KEY'] else "text"
 
 headers = {
  'Authorization': 'Bearer {}'.format(token),
  'Content-Type': 'application/json',
 }
- 
-api_url = 'https://translation.googleapis.com/language/translate/v2'
 
-results = []
+res = requests.post(url, headers=headers, json=data)
 
-for line in json.loads(json_lines):
-  params = {
-    'q': line[translate_json_key],
-    'source': from_language,
-    'target': to_language,
-    'format': 'text'
-  }
- 
-  res = requests.post(api_url, headers=headers, json=params)
-
-  trunslated_text = res.json()['data']['translations'][0]['translatedText']
-
-  line["translated_text"] = trunslated_text
-
-  results.append(line) 
- 
-print(json.dumps(results))
-
+print(json.dumps(res.json()))
+  
