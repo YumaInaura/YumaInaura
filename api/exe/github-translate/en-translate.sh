622165a9689af1ceea798713d633707d7a455291
Yumainaura <yuma.inaura@gmail.com>
2019-04-12 12:24:29 +0900

    no named commit

--- api/exe/github-translate/en-translate.sh
+++ api/exe/github-translate/en-translate.sh
@@ -5,7 +5,7 @@ set -eu
 base_dir=$(dirname "$0")
 source "${base_dir}/../../setting.sh"
 
-cat "$log_dir"/need-en-translate-issue.json \
+cat "$log_dir"/en-need-translate-issue.json \
   | "$base_dir"/en-translate.py \
   | tee "$log_dir"/en-translated.json
 
