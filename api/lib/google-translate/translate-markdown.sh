9f855ef366d5a4a2af495d70264892790fe40f1f
Yumainaura <yuma.inaura@gmail.com>
2019-04-12 09:45:12 +0900

    no named commit

--- api/lib/google-translate/translate-markdown.sh
+++ api/lib/google-translate/translate-markdown.sh
@@ -11,10 +11,18 @@ cat /dev/stdin \
   | tee "$log_dir"/seed.html
 
 cat "$log_dir"/seed.html \
+  | perl -e '@lines = (<STDIN>); $line = "@lines"; $line =~ s/\n/<newline>/sg; print $line;' \
+  | tee "$log_dir"/seed-formatted.html
+
+cat "$log_dir"/seed-formatted.html \
   | FORMAT=html ./translate.sh \
   | tee "$log_dir"/translated.html
 
 cat "$log_dir"/translated.html \
+  | perl -e '@lines = (<STDIN>); $line = "@lines"; $line =~ s/<newline>/\n/sg; print $line;' \
+  | tee "$log_dir"/translated-reverted.html
+
+cat "$log_dir"/translated-reverted.html \
   | jq . --raw-output \
   | reverse_markdown \
   | tee "$log_dir"/translated.md
