7150ec9f5b8ec5a1feba10bce6890a33cd1d486a
Yumainaura <yuma.inaura@gmail.com>
2019-04-12 18:04:45 +0900

    no named commit

--- api/exe/github-translate/create-all.sh
+++ api/exe/github-translate/create-all.sh
@@ -6,4 +6,5 @@ base_dir=$(dirname "$0")
 source "${base_dir}/../../setting.sh"
 
 eval "$base_dir"/en-create-issue.sh
+eval "$base_dir"/remove-label-from-issue-seed.sh
 eval "$base_dir"/remove-label-from-issue.sh
