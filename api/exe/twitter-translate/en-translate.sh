2a0f1e6951971d9ce60330324d369d0c768e58eb
Yumainaura <yuma.inaura@gmail.com>
2019-04-13 09:52:02 +0900

    no named commit

--- api/exe/twitter-translate/en-translate.sh
+++ api/exe/twitter-translate/en-translate.sh
@@ -11,6 +11,6 @@ if [ $(cat "$log_dir"/ja-timeline-recent.json | jq '. | length') -eq 0 ]; then
 fi
 
 cat "$log_dir"/ja-timeline-recent.json | \
-  TRANSLATE_JSON_KEY=full_text "$api_dir"/google-translate/translate-json.sh | \
+  TRANSLATE_JSON_KEY=full_text FORMAT=text "$api_dir"/google-translate/translate-json.sh | \
   tee "$log_dir"/en-translated.json 
 
