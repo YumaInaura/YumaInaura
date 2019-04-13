d4a41f570195bb94c2cccd3855f0e54dca77e426
Yumainaura <yuma.inaura@gmail.com>
2019-04-13 22:23:20 +0900

    no named commit

--- api/lib/twitter/update-profile.py
+++ api/lib/twitter/update-profile.py
@@ -4,19 +4,21 @@ import json, twitterauth, base64, sys
 
 twitter = twitterauth.twitter()
 
-api_url = 'https://api.twitter.com/1.1/account/update_profile_image.json'
 
 image_path = sys.argv[1]
 
 with open(image_path, "rb") as image_file:
   image_encoded_string = base64.b64encode(image_file.read())
 
-params = {
-  "image": image_encoded_string
-}
+api_url = 'https://api.twitter.com/1.1/account/update_profile_image.json?image=' + str(image_encoded_string)
 
-response = twitter.post(api_url, params=params)
+response = twitter.post(api_url)
 
-result = response.json()
+print(dir(response))
 
-print(json.dumps(result))
+#result = response.reason()
+
+print(response)
+
+#print(json.dumps(result))
+
