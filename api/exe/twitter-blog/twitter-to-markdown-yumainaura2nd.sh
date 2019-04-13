0914c42d02406552f3c9a570907bd1d58db73d04
Yumainaura <yuma.inaura@gmail.com>
2019-04-14 06:59:58 +0900

    no named commit

--- api/exe/twitter-blog/twitter-to-markdown-yumainaura2nd.sh
+++ api/exe/twitter-blog/twitter-to-markdown-yumainaura2nd.sh
@@ -17,7 +17,7 @@ ALL=1 "$api_dir"/twitter/timeline.py |
   > "$log_dir"/timeline-yumainaura-2nd.json
 
 cat "$log_dir"/timeline-yumainaura-2nd.json \
-  | OWN_USER_ID="$TWITTER_EN_USER_NAME" "$api_dir"/twitter/filter-own.py \
+  | OWN_USER_ID="$TWITTER_EN_USER_ID" "$api_dir"/twitter/filter-own.py \
   > "$log_dir"/timeline-own-tweet-yumainaura2nd.json
 
 cat "$log_dir"/timeline-own-tweet-yumainaura2nd.json \
