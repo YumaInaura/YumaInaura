bcde8857dd200d02b512767352043cb307be0365
Yumainaura <yuma.inaura@gmail.com>
2019-04-13 17:08:00 +0900

    no named commit

--- api/lib/json/concat-array.py
+++ api/lib/json/concat-array.py
@@ -5,7 +5,6 @@ import sys, json
 results = []
 
 for input_data in sys.argv[1:]:
-  data = json.loads(input_data)
-  results += data
+  results ++ json.loads(input_data)
 
 print(json.dumps(results))
