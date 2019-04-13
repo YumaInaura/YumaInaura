83d1122cfc3dffd024e7b76d48ea5bb53e7bdc43
Yumainaura <yuma.inaura@gmail.com>
2019-04-13 09:35:32 +0900

    no named commit

--- api/exe/twitter-blog/markdown-to-github-all.sh
+++ api/exe/twitter-blog/markdown-to-github-all.sh
@@ -8,7 +8,7 @@ base_dir=$(dirname "$0")
 source "${base_dir}/../../setting.sh"
 jst_date=$(TZ=Asia/Tokyo date --date='1 days ago' +'%y-%m-%d')
 
-eval "$base_dir"/markdown-to-github-diary-en.sh
-eval "$base_dir"/markdown-to-github-diary-samurai.sh
-eval "$base_dir"/markdown-to-github-diary.sh
+eval "$base_dir"/markdown-to-github-issug-en.sh
+eval "$base_dir"/markdown-to-github-issug-samurai.sh
+eval "$base_dir"/markdown-to-github-issug.sh
 
