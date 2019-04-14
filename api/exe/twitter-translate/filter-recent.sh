bb0271732f9eb664a1fa1ac357fe7173a524a5af
Yumainaura <yuma.inaura@gmail.com>
2019-04-14 09:49:09 +0900

    no named commit

--- api/exe/twitter-translate/filter-recent.sh
+++ api/exe/twitter-translate/filter-recent.sh
@@ -7,8 +7,8 @@ source "${base_dir}/../../setting.sh"
 
 interval_second=${INTERVAL:-60}
 
-start_unixtimestamp=$(($(date +%s) - $((3*60*60)) - $interval_second))
-end_unixtimestamp=$(($(date +%s) - $((3*60*60))))
+start_unixtimestamp=$(($(date +%s) - $((1*60*60)) - $interval_second))
+end_unixtimestamp=$(($(date +%s) - $((1*60*60))))
 
 cat "$log_dir"/ja-timeline-own.json | \
   "$api_dir"/twitter/filter-timestamp.py "$start_unixtimestamp" "$end_unixtimestamp" | \
