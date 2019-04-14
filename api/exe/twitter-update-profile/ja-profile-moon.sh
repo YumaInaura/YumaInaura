14ce7aaf6fe50d61561c87bea0bb9c86c8ff189a
Yumainaura <yuma.inaura@gmail.com>
2019-04-14 18:44:27 +0900

    no named commit

---
+++ api/exe/twitter-update-profile/ja-profile-moon.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -eu
+
+base_dir=$(dirname "$0")
+
+source "$base_dir"/../twitter-setting.sh
+source "$base_dir"/../../setting.sh
+source ~/.secret/env/twitter-yumainaura
+
+"$api_dir"/twitter/update-profile.py \
+  $(ls -1 "$base_dir"/image/*.jpg | grep moon | shuf -n 1)
+
