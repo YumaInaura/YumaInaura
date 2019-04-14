ff0cceeca61706be85e6abdbdefbd51e8a2de460
Yumainaura <yuma.inaura@gmail.com>
2019-04-14 18:50:19 +0900

    no named commit

--- api/exe/twitter-update-profile/ja-profile-moon.sh
+++ api/exe/twitter-update-profile/ja-profile-moon.sh
@@ -9,5 +9,5 @@ source "$base_dir"/../../setting.sh
 source ~/.secret/env/twitter-yumainaura
 
 "$api_dir"/twitter/update-profile.py \
-  $(ls -1 "$base_dir"/image/* | grep moon | shuf -n 1)
+  $(ls -1 "$base_dir"/image-moon/* | grep moon | shuf -n 1)
 
