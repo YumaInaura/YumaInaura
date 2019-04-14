933185f58d32c0ea7e4e6f85f8f7bb49a33014af
Yumainaura <yuma.inaura@gmail.com>
2019-04-14 18:20:22 +0900

    no named commit

--- api/exe/twitter-update-profile/en-profile-update.sh
+++ api/exe/twitter-update-profile/en-profile-update.sh
@@ -4,7 +4,9 @@ set -eu
 
 base_dir=$(dirname "$0")
 
+source "$base_dir"../twitter-setting.sh
 source ~/.secret/env/twitter-yumainaura2nd
 
-"$base_dir"/../..//lib/twitter/update-profile.py $(ls -1 "$base_dir"/../../lib/twitter/image/*.jpg | shuf -n 1)
+"$api_dir"/lib/twitter/update-profile.py \
+  $(ls -1 "$base_dir"/image/*.jpg | shuf -n 1)
 
