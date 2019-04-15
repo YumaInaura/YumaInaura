3114aea9a89c9fbadf6a9c484113c9ec39de1c96
Yumainaura <yuma.inaura@gmail.com>
2019-04-16 04:04:07 +0900

    no named commit

--- api/exe/qiita-translate/create-all.sh
+++ api/exe/qiita-translate/create-all.sh
@@ -9,6 +9,6 @@ source "${base_dir}/setting.sh"
 mkdir -p "$log_dir"
 
 cat "$log_dir"/"$QIITA_ITEMS_USER_NAME"_translated_test.json \
-  | "$api_dir"/qiita/item-post.py \
+  | "$api_dir"/qiita/item-post.sh \
   | tee "$log_dir"/"$QIITA_ITEMS_USER_NAME"_qiita_created.json
 
