64ce2442f922dcbd24cbd6a90e3aee6aa4664707
Yumainaura <yuma.inaura@gmail.com>
2019-04-16 03:44:03 +0900

    no named commit

--- api/exe/qiita-translate/fetch-all.sh
+++ api/exe/qiita-translate/fetch-all.sh
@@ -4,13 +4,11 @@ set -eu
 
 base_dir=$(dirname "$0")
 source "${base_dir}/../../setting.sh"
+source "${base_dir}/setting.sh"
 
 mkdir -p "$log_dir"
-
-export QIITA_ITEMS_USER_NAME=YumaInaura
 
 QIITA_ITEMS_ROUND=30 \
   "$api_dir"/qiita/items.py \
   | tee > "$log_dir"/"$QIITA_ITEMS_USER_NAME".json
-
 
