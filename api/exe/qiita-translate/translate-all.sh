da3e21d2d2a4ab4715442cffccaa918e693e9818
Yumainaura <yuma.inaura@gmail.com>
2019-04-16 08:02:23 +0900

    no named commit

--- api/exe/qiita-translate/translate-all.sh
+++ api/exe/qiita-translate/translate-all.sh
@@ -9,7 +9,6 @@ source "${base_dir}/setting.sh"
 mkdir -p "$log_dir"
 
 cat "$log_dir"/"$QIITA_ITEMS_USER_NAME".json \
-  | jq '[.[1]]' \
   | "$api_dir"/google-translate/translate-markdown.sh \
   | tee "$log_dir"/"$QIITA_ITEMS_USER_NAME"_translated_test.json
 
