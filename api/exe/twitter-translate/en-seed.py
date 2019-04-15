a8d7b730d74d30e678321cff39841d874497b834
Yumainaura <yuma.inaura@gmail.com>
2019-04-16 04:17:27 +0900

    no named commit

--- api/exe/twitter-translate/en-seed.py
+++ api/exe/twitter-translate/en-seed.py
@@ -8,7 +8,7 @@ results = []
 
 for tweet in tweets:
   seed = {}
-  seed['text'] = tweet['en_translated_text'][:280]
+  seed['text'] = tweet['en_translated_full_text'][:280]
   seed['attachment_url'] = tweet['url']
   #seed['text'] = tweet['translated_text'][:240] + ' '  + tweet['url']
   results.append(seed)
