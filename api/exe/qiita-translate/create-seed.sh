ddcd0dda425d44f56f4bbb7c6ae68d4fac70b16c
Yumainaura <yuma.inaura@gmail.com>
2019-04-16 08:25:23 +0900

    no named commit

--- api/exe/qiita-translate/create-seed.sh
+++ api/exe/qiita-translate/create-seed.sh
@@ -6,6 +6,7 @@ base_dir=$(dirname "$0")
 source "${base_dir}/../../setting.sh"
 source "${base_dir}/setting.sh"
 
-cat "$log_dir"/"$QIITA_ITEMS_USER_NAME"_translated.json \
-  | "$base_dir"/seed.py \
-  | tee "$log_dir"/"$QIITA_ITEMS_USER_NAME"_seed.json
+cat "$log_dir"/"$QIITA_ITEMS_USER_NAME"-translated.json \
+  | "$base_dir"/create-seed.py \
+  | tee "$log_dir"/"$QIITA_ITEMS_USER_NAME"-create-seed.json
+
