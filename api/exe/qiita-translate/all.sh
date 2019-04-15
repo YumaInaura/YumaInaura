af1a65b381fff1487ba890ee1522760be6ba007f
Yumainaura <yuma.inaura@gmail.com>
2019-04-16 03:37:59 +0900

    no named commit

---
+++ api/exe/qiita-translate/all.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eu
+
+base_dir=$(dirname "$0")
+source "${base_dir}/../../setting.sh"
+
+mkdir -p "$log_dir"
+rm -rf "$log_dir"/*
+
+eval "$base_dir"/fetch-all.sh
+eval "$base_dir"/format-all.sh
+eval "$base_dir"/translate-all.sh
+eval "$base_dir"/create-all.sh
+
