e40ccbb0e370782732a9865b14d38d92a5f0d9be
Yumainaura <yuma.inaura@gmail.com>
2019-04-16 09:07:04 +0900

    no named commit

--- api/exe/qiita-translate/create-seed.py
+++ api/exe/qiita-translate/create-seed.py
@@ -13,7 +13,7 @@ for translated in translateds:
   seed['title'] = translated['en_translated_title']
   seed['body'] = translated['en_translated_body']
   seed['body'] += "\n\n" + '# Original by' + "\n" +  '[' + translated['title'] + ']' + '(' + translated['url'] + ')'
-  seed['body'] += "\n\n" + '[About this translattion](https://qiita.com/YumaInaura/items/7f6fd1e9310a6816469a)'
+  seed['body'] += "\n\n" + '# About' + "\n\n" + '[About this translattion](https://qiita.com/YumaInaura/items/7f6fd1e9310a6816469a)'
 
   seed['tags'] = translated['tags']
 
