692cbb8ea98f5528fad3adf5992372d8cbd52b46
Yumainaura <yuma.inaura@gmail.com>
2019-04-17 18:49:34 +0900

    no named commit

--- api/lib/google-translate/translate-markdown.sh
+++ api/lib/google-translate/translate-markdown.sh
@@ -6,6 +6,8 @@ log_dir="$base_dir"/log
  
 mkdir -p "$log_dir"
 
+translate_json_key=${TRANSLATE_JSON_KEY:-text}
+
 cat /dev/stdin \
   | "${base_dir}"/markdown-to-html.py \
   > "$log_dir"/en-seed-html.json
@@ -13,7 +15,7 @@ cat /dev/stdin \
 cat "$log_dir"/en-seed-html.json \
   | \
     TOKEN=$("$base_dir"/get-token.sh) \
-    TRANSLATE_JSON_KEY=text \
+    TRANSLATE_JSON_KEY="$transkate_json_key" \
     FORMAT=html \
     FROM=ja \
     TO=en \
