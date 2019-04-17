f7781c51408ee6b283cd17c8a07bd2d5f7f18226
Yumainaura <yuma.inaura@gmail.com>
2019-04-17 19:04:47 +0900

    no named commit

--- api/exe/github-translate/en-translate.sh
+++ api/exe/github-translate/en-translate.sh
@@ -10,14 +10,8 @@ cat "$log_dir"/en-format.json \
     FORMAT=html \
     TOKEN=$("$api_dir"/google-translate/get-token.sh) \
     TRANSLATE_JSON_KEY=formatted_html \
-    TRANSLATED_JSON_KEY=en_translated_html \
+    TRANSLATED_JSON_KEY=en_translated_html,title \
       "$api_dir"/google-translate/translate-json.py \
-  | \
-    FORMAT=text \
-    TOKEN=$("$api_dir"/google-translate/get-token.sh) \
-    TRANSLATE_JSON_KEY=title \
-    TRANSLATED_JSON_KEY=en_translated_title \
-      "$api_dir"/google-translate/translate-json.py \
-  | tee "$log_dir"/en-translated.json
+ | tee "$log_dir"/en-translated.json
 
 
