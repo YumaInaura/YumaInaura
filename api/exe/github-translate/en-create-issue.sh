d795d0d79293622053317cf12ce2171c86e98b73
Yumainaura <yuma.inaura@gmail.com>
2019-04-17 21:28:14 +0900

    no named commit

--- api/exe/github-translate/en-create-issue.sh
+++ api/exe/github-translate/en-create-issue.sh
@@ -9,8 +9,6 @@ cat "$log_dir"/en-seed.json \
   | \
     USER_NAME=YumaInaura \
     API_KEY=$(cat ~/.secret/github-api-key) \
-    TITLE_JSON_KEY=en_translated_title \
-    BODY_JSON_KEY=en_translated_body \
       "$api_dir"/github/create-issue.py \
   | tee "$log_dir"/en-created-issue.json
 
