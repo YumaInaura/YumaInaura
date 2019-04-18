a28bab85314716b436b7fd1342b3dc8114bcbb4b
Yumainaura <yuma.inaura@gmail.com>
2019-04-18 09:34:10 +0900

    no named commit

--- api/lib/twitter/favorite-list.sh
+++ api/lib/twitter/favorite-list.sh
@@ -6,5 +6,5 @@ base_dir=$(dirname "$0")
 
 "$base_dir"/common.py \
   'https://api.twitter.com/1.1/favorites/list.json' \
-  '{ "user_name": "yumainaura", "include_entities": true, "count": 200 }'
+  '{ "include_entities": true, "count": 200 }'
 
