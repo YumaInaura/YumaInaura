6695652f45363684f60619229db4960f55f4a105
Yumainaura <yuma.inaura@gmail.com>
2019-04-18 15:12:00 +0900

    no named commit

--- api/lib/twitter/upstream-tweet-chain-by-show.py
+++ api/lib/twitter/upstream-tweet-chain-by-show.py
@@ -29,10 +29,10 @@ for i in range(1, MAX_ROUND+1):
   response = twitter.get(api_url, params=api_parameter)
   tweet = last_tweet = response.json()
 
-  print(tweet)
   if not tweet['in_reply_to_status_id_str']:
     break
   else:
     tweets.append(tweet)
 
 print(json.dumps(tweets))
+
