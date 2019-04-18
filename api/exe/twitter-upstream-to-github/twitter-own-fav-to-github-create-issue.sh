469f630a7fc09ee0e167ef9d036ef2702ac98719
Yumainaura <yuma.inaura@gmail.com>
2019-04-18 10:30:18 +0900

    no named commit

--- api/exe/twitter-upstream-to-github/twitter-own-fav-to-github-create-issue.sh
+++ api/exe/twitter-upstream-to-github/twitter-own-fav-to-github-create-issue.sh
@@ -7,6 +7,17 @@ base_dir=$(dirname "$0")
 source "${base_dir}/../../setting.sh"
 source "${base_dir}/../twitter-setting.sh"
 
+log_dir="$base_dir"/log
+mkdir -p "$log_dir"
+
 "$api_dir"/twitter/favorite-list.sh \
-  | jq '[.[] | select(.user.screen_name == "'"$TWITTER_JA_USER_NAME"'")]'
+  | jq '[.[] | select(.user.screen_name == "'"$TWITTER_JA_USER_NAME"'")]' \
+  | tee "$log_dir"/own-favorites.json
+
+tweet_id=$(cat "$log_dir"/own-favorites.json | jq -r '.[0].id_str')
+
+"$base_dir"/upstream-timeline.sh "$tweet_id" \
+  | tee "$log_dir"/tweet-upstream.json
+
+
 
