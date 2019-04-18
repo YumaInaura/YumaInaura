607fc50e30a966641058d8fadb6e75f8fb0c8368
Yumainaura <yuma.inaura@gmail.com>
2019-04-18 15:08:39 +0900

    no named commit

--- api/lib/twitter/upstream-tweet-chain-by-show.py
+++ api/lib/twitter/upstream-tweet-chain-by-show.py
@@ -12,9 +12,17 @@ tweets = []
 
 MAX_ROUND = int(os.environ.get('ROUND')) if os.environ.get('ROUND') else 30
 
+tweet_url_or_id = sys.argv[1]
+
+if re.search(r'\d+$', tweet_url_or_id):
+  tweet_id = tweet_url_or_id
+else:
+  tweet_id = re.search(r'/status/(?P<id>\d+)', tweet_url_or_id)['id']
+
+
 for i in range(1, MAX_ROUND+1):
   api_parameter = {
-    "id": last_tweet['in_reply_to_status_id_str'] if last_tweet and last_tweet['in_reply_to_status_id_str'] else sys.argv[1],
+    "id": last_tweet['in_reply_to_status_id_str'] if last_tweet and last_tweet['in_reply_to_status_id_str'] else tweet_id,
   }
   
   api_url = "https://api.twitter.com/1.1/statuses/show.json"
