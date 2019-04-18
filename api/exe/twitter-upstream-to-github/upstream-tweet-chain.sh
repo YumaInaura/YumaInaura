fabcf9bb44c582ed4c44e15aee6afdaff48cc16f
Yumainaura <yuma.inaura@gmail.com>
2019-04-18 19:57:37 +0900

    no named commit

--- api/exe/twitter-upstream-to-github/upstream-tweet-chain.sh
+++ api/exe/twitter-upstream-to-github/upstream-tweet-chain.sh
@@ -8,7 +8,7 @@ source "${base_dir}/../../setting.sh"
 log_dir="$base_dir"/log
 mkdir -p "$log_dir"
 
-tweet_id=$cat("$log_dir"/own-favorite-tweet-id.log)
+tweet_id=$(cat "$log_dir"/own-favorite-tweet-id.log)
 
 eval "$api_dir"/twitter/upstream-tweet-chain.py "$tweet_url" \
   > "$log_dir"/upstream-tweets.json
