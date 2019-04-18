52566139af5166c7caca8ea6f91a5f247efa4482
Yumainaura <yuma.inaura@gmail.com>
2019-04-18 15:13:10 +0900

    no named commit

--- api/lib/twitter/upstream-tweet-chain-by-show.py
+++ api/lib/twitter/upstream-tweet-chain-by-show.py
@@ -22,6 +22,7 @@ else:
 for i in range(1, MAX_ROUND+1):
   api_parameter = {
     "id": last_tweet['in_reply_to_status_id_str'] if last_tweet and last_tweet['in_reply_to_status_id_str'] else tweet_id,
+    "tweet_mode" : "extended"
   }
   
   api_url = "https://api.twitter.com/1.1/statuses/show.json"
