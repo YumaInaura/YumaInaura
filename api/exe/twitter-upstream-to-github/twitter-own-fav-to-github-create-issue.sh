4291fdab202c31df94cf4b07bf214d77ecec5291
Yumainaura <yuma.inaura@gmail.com>
2019-04-18 09:56:10 +0900

    no named commit

--- api/exe/twitter-upstream-to-github/twitter-own-fav-to-github-create-issue.sh
+++ api/exe/twitter-upstream-to-github/twitter-own-fav-to-github-create-issue.sh
@@ -5,16 +5,3 @@ set -eu
 base_dir=$(dirname "$0")
 source "${base_dir}/../../setting.sh"
 
-log_dir="$base_dir"/log
-
-cat "$log_dir"/timeline.json \
-  | jq reverse \
-  | "$api_dir"/twitter/format-customed-mark.py \
-  | "$api_dir"/twitter/markdown.py \
-  > "$log_dir"/github-body.md
-
-cat "$log_dir"/timeline.json \
-  | jq --raw-output '.[0].full_text' \
-   > "$log_dir"/github-title.txt
-
-
