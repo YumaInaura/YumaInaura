7ff31b33a8c7a47084ef287b071402975a56bd47
Yumainaura <yuma.inaura@gmail.com>
2019-04-18 10:42:53 +0900

    no named commit

---
+++ api/exe/twitter-upstream-to-github/upstream-timeline-by-show.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eu
+
+base_dir=$(dirname "$0")
+source "${base_dir}/../../setting.sh"
+
+log_dir="$base_dir"/log
+mkdir -p "$log_dir"
+
+tweet_url="$1"
+
+eval "$api_dir"/twitter/upstream-tweet-chain-by-show.py "$tweet_url" \
+  > "$log_dir"/timeline.json
+
