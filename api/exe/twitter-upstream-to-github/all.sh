e6efbc5b7bdb96b5dd0628e507d23c76d0386f01
Yumainaura <yuma.inaura@gmail.com>
2019-04-19 09:06:21 +0900

    no named commit

--- api/exe/twitter-upstream-to-github/all.sh
+++ api/exe/twitter-upstream-to-github/all.sh
@@ -6,7 +6,7 @@ base_dir=$(dirname "$0")
 source "${base_dir}/../../setting.sh"
 
 "$base_dir"/twitter-own-fav.sh
-"$base_dir"/upstream-timeline.sh
+"$base_dir"/upstream-tweet-chain.sh
 "$base_dir"/markdown.sh
 "$base_dir"/github-create-issue.sh
 
