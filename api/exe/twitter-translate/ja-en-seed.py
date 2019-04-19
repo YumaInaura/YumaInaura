898dd55721b7b10a913dd04cc25f060238cb1125
Yumainaura <yuma.inaura@gmail.com>
2019-04-19 16:50:40 +0900

    no named commit

--- api/exe/twitter-translate/ja-en-seed.py
+++ api/exe/twitter-translate/ja-en-seed.py
@@ -27,17 +27,17 @@ for tweet in tweets:
   quoted_url_regexp = re.compile('https://t.co/\w+$')
   quoted_url_matched =  re.search(quoted_url_regexp, tweet['en_translated_full_text'])
 
+  hashtag_pattern = re.compile(r'\b(?<!#)(ruby|perl|python|shell|engineer|programming|programmer)\b', re.IGNORECASE)
+  seed['text'] = re.sub(hashtag_pattern, "#\\1", tweet['en_translated_full_text']))
+
   if tweet['is_quote_status']:
     ref_url = quoted_url_matched.group(0)
-    ref_url_deleted_text = re.sub(quoted_url_regexp, '', tweet['en_translated_full_text'])
+    ref_url_deleted_text = re.sub(quoted_url_regexp, '', seed['text'])
     seed['text'] = ref_url_deleted_text[:254] + (ref_url_deleted_text[254:] and '‥')  + "\n" + ref_url
   else:
     # seed['attachment_url'] = tweet['url']
-    seed['text'] = re.sub(quoted_url_regexp, '', tweet['en_translated_full_text'])
+    seed['text'] = re.sub(quoted_url_regexp, '', seed['text'])
     seed['text'] = seed['text'][:279] + (ref_url_deleted_text[279:] and '‥')
-
-  hashtag_pattern = re.compile(r'\b(?<!#)(ruby|perl|python)\b', re.IGNORECASE)
-  seed['text'] = re.sub(hashtag_pattern, "#\\1", seed['text'])
 
   seed['in_reply_to_status_id'] = tweet['id_str']
 
