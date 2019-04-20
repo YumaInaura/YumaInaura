7ac9ce5317926c956557dfbd44e6a5916a1a7e85
Yumainaura <yuma.inaura@gmail.com>
2019-04-20 13:26:42 +0900

    no named commit

--- api/exe/twitter-translate/ja-en-seed.sh
+++ api/exe/twitter-translate/ja-en-seed.sh
@@ -14,6 +14,13 @@ cat "$log_dir"/en-translated.json | \
   > "$log_dir"/ja-en-seed.json 
 
 cat "$log_dir"/en-translated.json | \
+  DICTIONARY_JSON_KEY=id \
+  HASHTAG_JSON_KEY=full_text \
+    "$api_dir"/twitter/add-hashtag.py \
+      "$base_dir"/../../exe/qiita-tags/log/english-tags.json \
+  > "$log_dir"/ja-en-seed.json 
+
+cat "$log_dir"/en-translated.json | \
   "$base_dir"/ja-en-seed.py | \
   tee "$log_dir"/ja-en-seed.json 
 
