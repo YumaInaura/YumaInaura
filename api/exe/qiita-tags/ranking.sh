1994a1a0a1e513c52b3958d386011b8cdbf21385
Yumainaura <yuma.inaura@gmail.com>
2019-04-20 18:44:09 +0900

    no named commit

--- api/exe/qiita-tags/ranking.sh
+++ api/exe/qiita-tags/ranking.sh
@@ -6,6 +6,6 @@ base_dir=$(dirname "$0")
 source "${base_dir}/../../setting.sh"
 
 cat "$log_dir"/tags.json \
-  | jq -r '.[] | "+ [" + .id + "](https://qiita.com/tags/" + .id + ") " + (.items_count | tostring) + " items"' \
+  | jq -r '.[] | "1. [" + .id + "](https://qiita.com/tags/" + .id + ") " + (.items_count | tostring) + " items"' \
   | tee "$log_dir"/ranking.md
 
