7ba3126aeb367ce706f234ac9a2dbda68bfd47e3
Yumainaura <yuma.inaura@gmail.com>
2019-04-20 11:31:23 +0900

    no named commit

--- api/exe/qiita-tags/all.sh
+++ api/exe/qiita-tags/all.sh
@@ -8,4 +8,10 @@ source "${base_dir}/../../setting.sh"
 mkdir -p "$log_dir"
 rm -rf "$log_dir"/*
 
+QIITA_TAGS_ROUND=30 \
+  "$api_dir"/qiita/tags.py
+
+cat "$log_dir"/tags.json | \
+  jq '.[] | select(.id | match("^[a-zA-z][a-zA-z0-9]+$"))' |
+  > "$log_dir"/english-tags.json
 
