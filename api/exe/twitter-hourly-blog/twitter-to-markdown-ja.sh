c839f8ebf99b8a1e7e1ad82d2de4dac43ab85893
Yumainaura <yuma.inaura@gmail.com>
2019-04-24 09:11:25 +0900

    no named commit

--- api/exe/twitter-hourly-blog/twitter-to-markdown-ja.sh
+++ api/exe/twitter-hourly-blog/twitter-to-markdown-ja.sh
@@ -27,8 +27,8 @@ start_unixtimestamp=$(($(date +%s) - $((60)) - $interval_second))
 end_unixtimestamp=$(($(date +%s) - $((60))))
 
 cat "$log_dir"/ja-timeline-own.json | \
-  "$api_dir"/twitter/filter-timestamp.py "$start_unixtimestamp" "$end_unixtimestamp" | \
-  tee "$log_dir"/ja-timeline-recent.json
+  "$api_dir"/twitter/filter-timestamp.py "$start_unixtimestamp" "$end_unixtimestamp" \
+  | tee "$log_dir"/ja-timeline-recent.json
 
 cat "$log_dir"/ja-timeline-recent.json | \
   "$api_dir"/twitter/format-customed-mark.py \
