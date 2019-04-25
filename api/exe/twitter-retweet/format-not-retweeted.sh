5b049d1a67cb4d31ed66c665668fc4dce9c94b26
Yumainaura <yuma.inaura@gmail.com>
2019-04-25 15:59:01 +0900

    no named commit

--- api/exe/twitter-retweet/format-not-retweeted.sh
+++ api/exe/twitter-retweet/format-not-retweeted.sh
@@ -11,7 +11,7 @@ cat "$log_dir"/timeline-"$TWITTER_JA_USER_NAME".json \
   | jq '[.[] | select(.retweeted | not)]' \
   | tee "$log_dir"/not-retweeted-"$TWITTER_JA_USER_NAME".json
 
-cat "$log_dir"/retweeted-"$TWITTER_JA_USER_NAME".json \
+cat "$log_dir"/not-retweeted-"$TWITTER_JA_USER_NAME".json \
   | jq -r '.[].id_str' \
   | shuf \
   | head -n 1 \
