6262950531818905e024eb5a247c083a141eb5a4
Yumainaura <yuma.inaura@gmail.com>
2019-04-25 11:17:53 +0900

    no named commit

---
+++ api/exe/twitter-retweet/twitter-to-markdown.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+set -eu
+
+
+base_dir=$(dirname "$0")
+
+source "${base_dir}/../../setting.sh"
+source "${base_dir}/../twitter-setting.sh"
+jst_date=$(TZ=Asia/Tokyo date --date='1 days ago' +'%Y-%m-%d')
+
+mkdir -p "$log_dir"
+rm -rf "$log_dir"/*
+
+source ~/.secret/env/twitter-yumainaura
+
+tweet_border=3
+
+interval_second=${INTERVAL:-3600}
+start_unixtimestamp=$(($(date +%s) - $((3*60*60)) - $interval_second))
+end_unixtimestamp=$(($(date +%s) - $((3*60*60))))
+
+ALL=1 \
+  "$api_dir"/twitter/timeline.py \
+  > "$log_dir"/timeline-"$TWITTER_JA_USER_NAME".json
+
+cat "$log_dir"/timeline-"$TWITTER_JA_USER_NAME".json \
+  | \
+  OWN_USER_ID="$TWITTER_JA_USER_ID" \
+    "$api_dir"/twitter/filter-own.py \
+  > "$log_dir"/own-"$TWITTER_JA_USER_NAME".json
+
+cat "$log_dir"/own-"$TWITTER_JA_USER_NAME".json \
+  | "$api_dir"/twitter/filter-timestamp.py "$start_unixtimestamp" "$end_unixtimestamp" \
+  > "$log_dir"/recent-"$TWITTER_JA_USER_NAME".json
+
+cat "$log_dir"/recent-"$TWITTER_JA_USER_NAME".json \
+  | "$api_dir"/twitter/format-customed-mark.py \
+  > "$log_dir"/formatted-"$TWITTER_JA_USER_NAME".json
+
+cat "$log_dir"/formatted-"$TWITTER_JA_USER_NAME".json \
+  | jq '[.[] | select(.in_reply_to_status_id == null)]' \
+  > "$log_dir"/countable-"$TWITTER_JA_USER_NAME".json
+
+cat "$log_dir"/countable-"$TWITTER_JA_USER_NAME".json \
+  | jq 'sort_by(.favorite_count) | reverse' \
+  > "$log_dir"/favorite-"$TWITTER_JA_USER_NAME".json
+
+countable_tweet_num=$(cat "$log_dir"/countable-"$TWITTER_JA_USER_NAME".json | jq length)
+if [ $countable_tweet_num -lt $tweet_border ]; then
+  echo Tweets num under "$countable_tweet_num" / "$tweet_border" 
+  exit 1
+fi
+
+echo \
+  $(
+    cat "$log_dir"/favorite-"$TWITTER_JA_USER_NAME".json \
+      | jq -r '.[0].full_text_without_quoted_url' \
+      | head -n 1
+  ) \
+  " $jst_date on Twitter" \
+  | tee "$log_dir"/github-issue-title-"$TWITTER_JA_USER_NAME".txt
+
+cat "$log_dir"/formatted-"$TWITTER_JA_USER_NAME".json \
+  | "$api_dir"/twitter/markdown.py \
+  | tee "$log_dir"/github-issue-body-"$TWITTER_JA_USER_NAME".md
+
