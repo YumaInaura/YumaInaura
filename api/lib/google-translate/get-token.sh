1f81520e3ec8fe618ec91711428de943f3ca47f6
YumaInaura <yuma.inaura@gmail.com>
2019-04-27 15:04:02 +0900

    no named commit

--- api/lib/google-translate/get-token.sh
+++ api/lib/google-translate/get-token.sh
@@ -1,7 +1,2 @@
-if [ $(uname -s) = "Darwin" ]; then
-  GOOGLE_APPLICATION_CREDENTIALS=~/.secret/google-service-credential.json gcloud auth application-default print-access-token
-else
-  docker exec gcp /bin/bash ./get-token.sh
-fi
-
+docker exec gcp /bin/bash ./get-token.sh
 
