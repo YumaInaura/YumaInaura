db5dd8bcdfd4c12468f227fde30fa44916d67f23
YumaInaura <yuma.inaura@gmail.com>
2019-04-27 14:43:14 +0900

    no named commit

--- api/exe/twitter-translate/ja-en-seed.py
+++ api/exe/twitter-translate/ja-en-seed.py
@@ -32,7 +32,8 @@ for tweet in tweets:
   if tweet['is_quote_status']:
     ref_url = quoted_url_matched.group(0)
     ref_url_deleted_text = re.sub(quoted_url_regexp, '', seed['text'])
-    seed['text'] = ref_url_deleted_text[:254] + (ref_url_deleted_text[254:] and '‥')  + "\n" + ref_url
+    seed['text'] = ref_url_deleted_text[:279] + (ref_url_deleted_text[279:] and '‥') 
+    # seed['text'] = ref_url_deleted_text[:254] + (ref_url_deleted_text[254:] and '‥')  + "\n" + ref_url
   else:
     seed['attachment_url'] = tweet['url']
     seed['text'] = re.sub(quoted_url_regexp, '', seed['text'])
