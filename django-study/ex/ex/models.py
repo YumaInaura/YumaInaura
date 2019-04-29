35007c4106342b37fc2498c64122f3cf1a861638
YumaInaura <yuma.inaura@gmail.com>
2019-04-29 12:52:51 +0900

    no named commit

--- django-study/ex/ex/models.py
+++ django-study/ex/ex/models.py
@@ -2,3 +2,9 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+
+class Hello(models.Model):
+    your_name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return "<{0}>".format(self.your_name)
