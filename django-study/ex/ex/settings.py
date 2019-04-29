6056dd9e08ac76576a6327fbe047ad095c5c0b12
YumaInaura <yuma.inaura@gmail.com>
2019-04-29 10:38:38 +0900

    no named commit

--- django-study/ex/ex/settings.py
+++ django-study/ex/ex/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'ex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '101010',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
