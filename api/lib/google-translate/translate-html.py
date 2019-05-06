305288d6390e06de52fb09cea090fd157f819281
YumaInaura <yuma.inaura@gmail.com>
2019-05-06 11:07:59 +0900

    no named commit

--- api/lib/google-translate/translate-html.py
+++ api/lib/google-translate/translate-html.py
@@ -2,13 +2,28 @@
 
 # -*- coding: utf-8 -*-
 
-import os, sys, requests, json, fileinput, re
+import os, sys, requests, json, fileinput, re, uuid
 
 resource_message = sys.stdin.read()
 
 from_language = os.environ.get('FROM') if os.environ.get('FROM') else 'ja'
 to_language = os.environ.get('TO') if os.environ.get('TO') else 'en'
 translate_format = 'html'
+
+codeblocks = re.findall(r'<pre>(?:<code>)?.+?(?:</code>)?</pre>', resource_message, re.DOTALL)
+
+table = {}
+
+print(codeblocks)
+for codeblock in codeblocks:
+  hex_hash = uuid.uuid4().hex
+  table[hex_hash] = codeblock
+
+  resource_message = re.sub(re.compile(codeblock), '<hex>' + hex_hash + '</hex>', resource_message)
+
+print(table) 
+print(resource_message) 
+exit()
 
 data = {
   'q': resource_message,
