e010da777d19d2a46f6f43f4e63ce6da0a97338b
YumaInaura <yuma.inaura@gmail.com>
2019-05-06 11:30:09 +0900

    no named commit

--- api/lib/google-translate/translate-html.py
+++ api/lib/google-translate/translate-html.py
@@ -11,49 +11,48 @@ to_language = os.environ.get('TO') if os.environ.get('TO') else 'en'
 translate_format = 'html'
 
 def hash_codeblockes(resource_message):
-  table = {}
+  codeblocks_table = {}
 
   codeblocks = re.findall(r'<pre>(?:<code>)?(.+?)(?:</code>)?</pre>', resource_message, re.DOTALL)
 
   for codeblock in codeblocks:
     hex_hash = uuid.uuid4().hex
-    table[hex_hash] = codeblock
+    codeblocks_table[hex_hash] = codeblock
   
     resource_message = re.sub(re.compile(codeblock), '<hex>' + hex_hash + '</hex>', resource_message)
 
-  return(table, resource_message)
+  return(codeblocks_table, resource_message)
 
-
-def revert_codeblockes(resource_message, table):
-  for t in table:
-    code = table[t]
+def revert_codeblockes(resource_message, codeblocks_table):
+  for t in codeblocks_table:
+    code = codeblocks_table[t]
   
     resource_message = re.sub(r'<hex>.+?</hex>', code, resource_message)
 
   return resource_message
 
-table, resource_message = hash_codeblockes(resource_message)
-resource_message = revert_codeblockes(resource_message, table)
+def google_translate(resource_message):
+  data = {
+    'q': resource_message,
+    'source': from_language,
+    'target': to_language,
+    'format': translate_format
+  }
+  
+  url = 'https://translation.googleapis.com/language/translate/v2'
+  token = os.environ['TOKEN']
+  
+  headers = {
+   'Authorization': 'Bearer {}'.format(token),
+   'Content-Type': 'application/json',
+  }
+  
+  res = requests.post(url, headers=headers, json=data)
+
+  return(res.json()['data']['translations'][0]['translatedText'])
+
+codeblocks_table, resource_message = hash_codeblockes(resource_message)
+resource_message = google_translate(resource_message)
+resource_message = revert_codeblockes(resource_message, codeblocks_table)
 
 print(resource_message)
-
-
-data = {
-  'q': resource_message,
-  'source': from_language,
-  'target': to_language,
-  'format': translate_format
-}
-
-url = 'https://translation.googleapis.com/language/translate/v2'
-token = os.environ['TOKEN']
-
-headers = {
- 'Authorization': 'Bearer {}'.format(token),
- 'Content-Type': 'application/json',
-}
-
-res = requests.post(url, headers=headers, json=data)
-
-print(json.dumps(res.json()))
-  
