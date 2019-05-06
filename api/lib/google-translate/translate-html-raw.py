856eee70541a9c285c636cea7cd4b186dd12275f
YumaInaura <yuma.inaura@gmail.com>
2019-05-06 11:42:08 +0900

    no named commit

--- api/lib/google-translate/translate-html-raw.py
+++ api/lib/google-translate/translate-html-raw.py
@@ -10,18 +10,23 @@ from_language = os.environ.get('FROM') if os.environ.get('FROM') else 'ja'
 to_language = os.environ.get('TO') if os.environ.get('TO') else 'en'
 translate_format = 'html'
 
-def hash_codeblockes(resource_message):
+def codeblock_tables(resource_message):
   codeblocks_table = {}
 
-  codeblocks = re.findall(r'(<pre>(?:<code>)?(?:.+?)(?:</code>)?</pre>)', resource_message, re.DOTALL)
+  codeblocks = re.findall(r'<pre>(?:<code>)?(.+?)(?:</code>)?</pre>', resource_message, re.DOTALL)
 
   for codeblock in codeblocks:
     hex_hash = uuid.uuid4().hex
     codeblocks_table[hex_hash] = codeblock
   
+  return codeblocks_table
+
+def convert_codeblocks(resouce_message, codeblocks_table):
+  for t in codeblocks_table:
+    code = codeblocks_table[t]
     resource_message = re.sub(re.compile(codeblock), '<hex>' + hex_hash + '</hex>', resource_message)
 
-  return(codeblocks_table, resource_message)
+  return resource_message
 
 def revert_codeblockes(resource_message, codeblocks_table):
   for t in codeblocks_table:
@@ -51,8 +56,9 @@ def google_translate(resource_message):
 
   return(res.json()['data']['translations'][0]['translatedText'])
 
-codeblocks_table, resource_message = hash_codeblockes(resource_message)
-print(resource_message); exit()
+codeblocks_table = codeblock_tables(resource_message)
+#print(resource_message); exit()
+print(codeblocks_table); exit()
 #resource_message = revert_codeblockes(resource_message, codeblocks_table)
 
 # print(codeblocks_table); print(resource_message); exit()
