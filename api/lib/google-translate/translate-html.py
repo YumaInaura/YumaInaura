4bc46a197252d6bbdcb33e71564d6bdd621a457f
YumaInaura <yuma.inaura@gmail.com>
2019-05-06 09:24:15 +0900

    no named commit

--- api/lib/google-translate/translate-html.py
+++ api/lib/google-translate/translate-html.py
@@ -4,15 +4,11 @@
 
 import os, sys, requests, json, fileinput, re
 
-#resource_message = ''
-#for text in sys.stdin.readlines():
-#  resource_message += re.sub(r'\\n', "\n", text)
-
 resource_message = sys.stdin.read()
 
 from_language = os.environ.get('FROM') if os.environ.get('FROM') else 'ja'
 to_language = os.environ.get('TO') if os.environ.get('TO') else 'en'
-translate_format = os.environ.get('FORMAT') if os.environ.get('FORMAT') else 'text'
+translate_format = 'html'
 
 data = {
   'q': resource_message,
