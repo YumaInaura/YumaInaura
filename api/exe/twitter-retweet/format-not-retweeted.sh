9ce38f9df4729ad96adf5b7c43049ee1ca098056
YumaInaura <yuma.inaura@gmail.com>
2019-05-10 08:15:41 +0900

    no named commit

--- api/exe/twitter-retweet/format-not-retweeted.sh
+++ api/exe/twitter-retweet/format-not-retweeted.sh
@@ -8,8 +8,8 @@ source "${base_dir}/../../setting.sh"
 source "${base_dir}/../twitter-setting.sh"
 
 cat "$log_dir"/timeline-"$TWITTER_JA_USER_NAME".json \
-  | jq '[.[] | select(.retweeted | not)]' \
-  | jq '[.[] | select(.favorite_count >= 3)]' \
+#  | jq '[.[] | select(.retweeted | not)]' \
+#  | jq '[.[] | select(.favorite_count >= 3)]' \
   | jq '[.[] | select(.in_reply_to_user_id_str == null)]' \
   | tee "$log_dir"/not-retweeted-"$TWITTER_JA_USER_NAME".json
 
