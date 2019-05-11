83d466f012da7a5bce3878efce644181f7aae0f8
YumaInaura <yuma.inaura@gmail.com>
2019-05-11 10:38:00 +0900

    no named commit

--- api/exe/twitter-partly-blog/twitter-profiles.sh
+++ api/exe/twitter-partly-blog/twitter-profiles.sh
@@ -7,6 +7,8 @@ base_dir=$(dirname "$0")
 source "${base_dir}/../../setting.sh"
 source "${base_dir}/../twitter-setting.sh"
 
+rm -f "$log_dir"/all-user-profiles-"$TWITTER_JA_USER_NAME".md
+
 cat "$log_dir"/recent-"$TWITTER_JA_USER_NAME".json \
   | jq '[.[] | select(.quoted_status.entities.user_mentions)]' \
   | jq -r '.[].quoted_status.entities.user_mentions[].screen_name' \
@@ -16,6 +18,7 @@ cat "$log_dir"/recent-"$TWITTER_JA_USER_NAME".json \
 
 for display_name in $(cat "$log_dir"/quoted-user-screen-names-"$TWITTER_JA_USER_NAME".txt); do
   "$api_dir"/twitter/user-show.sh "$display_name" \
-  | "$api_dir"/twitter/markdown-user.py
+  | "$api_dir"/twitter/markdown-user.py \
+  >> "$log_dir"/all-user-profiles-"$TWITTER_JA_USER_NAME".md
 done
 
