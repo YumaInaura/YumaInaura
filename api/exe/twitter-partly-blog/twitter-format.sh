76316a43cef03c162303a74bf7c85714ff755afb
YumaInaura <yuma.inaura@gmail.com>
2019-05-11 09:33:15 +0900

    no named commit

--- api/exe/twitter-partly-blog/twitter-format.sh
+++ api/exe/twitter-partly-blog/twitter-format.sh
@@ -2,28 +2,16 @@
 
 set -eu
 
-
 base_dir=$(dirname "$0")
 
 source "${base_dir}/../../setting.sh"
 source "${base_dir}/../twitter-setting.sh"
-jst_date=$(TZ=Asia/Tokyo date --date='1 days ago' +'%Y-%m-%d')
-
-mkdir -p "$log_dir"
-rm -rf "$log_dir"/*
-
-source ~/.secret/env/twitter-yumainaura
 
 tweet_border=3
 
 interval_second=${INTERVAL:-3600}
 start_unixtimestamp=$(($(date +%s) - $((3*60*60)) - $interval_second))
 end_unixtimestamp=$(($(date +%s) - $((3*60*60))))
-
-ALL=1 \
-ROUND=1 \
-  "$api_dir"/twitter/timeline.sh \
-  > "$log_dir"/timeline-"$TWITTER_JA_USER_NAME".json
 
 cat "$log_dir"/timeline-"$TWITTER_JA_USER_NAME".json \
   | \
