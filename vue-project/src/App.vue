f7d73180ed3e61314ffb04e496c3d0e8c0c06896
YumaInaura <yuma.inaura@gmail.com>
2020-04-29 19:02:18 +0900

    update

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Foo/>
+    <Foo msg="Wow"/>
+    <Foo/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Foo from './components/Foo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Foo
   }
 }
 </script>
---
+++ vue-project/src/components/Foo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      default: 'Ya!',
+      required: false
+    }
+  }
+}
+</script>
