2afd506aa618e930ea34692b673b4d21af507e52
YumaInaura <yuma.inaura@gmail.com>
2020-05-02 16:35:22 +0900

    update

--- nuxt-project/nuxt.config.js
+++ nuxt-project/nuxt.config.js
@@ -38,6 +38,7 @@ export default {
   ** Nuxt.js modules
   */
   modules: [
+    '@nuxtjs/axios',
   ],
   /*
   ** Build configuration
---
+++ nuxt-project/pages/axios.vue
@@ -0,0 +1,26 @@
+// https://axios.nuxtjs.org/setup.html#install
+
+// https://ma-vericks.com/nuxt-axios/
+
+// $ yarn add @nuxtjs/axios
+
+// Edit nuxt.config.js
+//
+// modules: [
+//   '@nuxtjs/axios',
+// ],
+
+<template>
+  <h1>
+    {{ response }}
+  </h1>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const response = await $axios.$get('https://example.com')
+    return { response }
+  }
+}
+</script>
