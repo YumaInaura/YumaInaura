c3813b6aada45da3d1ffaac174c78feff126b301
YumaInaura <yuma.inaura@gmail.com>
2020-05-04 13:24:19 +0900

    update

---
+++ nuxt-project/pages/assets-json.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>
+      assset API
+    </h1>
+    <a href="~/assets/example.json">A</a>
+    <h2>Users</h2>
+    <div>
+      {{ users }}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  // async asyncData ({ $axios }) {
+  //   const users = await $axios.$get(`/_nuxt/assets/example.json`)
+  //   return { user, users }
+  // }
+}
+</script>
---
+++ nuxt-project/pages/static-image.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>
+      static Image
+    </h1>
+    <img src="/image.jpg">
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
