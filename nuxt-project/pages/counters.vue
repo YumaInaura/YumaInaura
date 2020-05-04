5db4e0ca800c2b6d78bd183540d42c7bc2f4a2b7
YumaInaura <yuma.inaura@gmail.com>
2020-05-05 07:16:20 +0900

    update

---
+++ nuxt-project/pages/counters.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>
+      parentCount: {{ parentCount }}
+    </h2>
+    <input type="button" @click="incrementParentCounter" value="Increment Parent">
+    <input type="button" @click="reset" value="reset Parent">
+<!--
+    <Counter :count="parentCount"/>
+    <Counter :count="parentCount"/>
+ -->
+    <LocalCounter :count="parentCount"/>
+    <LocalCounter :count="parentCount"/>
+  </div>
+</template>
+
+<script>
+import Counter from '~/components/Counter.vue'
+import LocalCounter from '~/components/LocalCounter.vue'
+
+export default {
+  data () {
+    return {
+      parentCount: 1
+    }
+  },
+  components: {
+    Counter,
+    LocalCounter
+  },
+  methods: {
+    incrementParentCounter (e) {
+      this.parentCount += 1
+    },
+    reset (e) {
+      this.parentCount = 1
+    },
+  }
+}
+</script>
