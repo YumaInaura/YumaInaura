9513cc86d03ae4888d74f516358c6d3c40d31e85
YumaInaura <yuma.inaura@gmail.com>
2020-05-05 16:35:34 +0900

    update

---
+++ nuxt-project/pages/state-counters.vue
@@ -0,0 +1,25 @@
+
+// https://nuxtjs.org/guide/vuex-store/
+
+// store/increments.js
+
+<template>
+  <div>
+    <div>
+      <stateCounter/>
+      <stateCounter/>
+      <stateCounter/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import StateCounter from '~/components/stateCounter.vue'
+
+export default {
+  components: {
+    StateCounter
+  }
+}
+</script>
---
+++ nuxt-project/pages/vuex-map-state.vue
@@ -0,0 +1,28 @@
+// https://nuxtjs.org/guide/vuex-store
+
+<template>
+  <div>{{ count }}</div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+
+export default {
+  computed: {
+    count () {
+      return this.$store.state.counters.count
+    }
+  },
+  methods: {
+    addTodo (e) {
+      this.$store.commit('counters/count', e.target.value)
+    }
+  }
+}
+</script>
+
+<style>
+.done {
+  text-decoration: line-through;
+}
+</style>
