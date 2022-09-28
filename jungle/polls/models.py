5a9f0ef056a11314131503a0fc9457a801a389a6
YumaInaura <yuma.inaura@gmail.com>
2022-09-28 20:40:32 +0900

    tutorial

--- jungle/polls/models.py
+++ jungle/polls/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from django.db import models
-
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
