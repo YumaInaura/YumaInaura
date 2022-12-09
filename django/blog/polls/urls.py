0e3f92fcd9bdc2562d6866107283bfc452b4d886
YumaInaura <yuma.inaura@gmail.com>
2022-12-09 23:16:18 +0900

    detail

--- django/blog/polls/urls.py
+++ django/blog/polls/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 
 from . import views
 
-app_name = 'polls'
+app_name = 'articles'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:article_id>/', views.detail, name='detail'),
+#    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
     path('<int:article_id>/comment/', views.comment, name='comment'),
 ]
