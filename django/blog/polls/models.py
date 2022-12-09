3f4f7c7230f5173c6f8c87a227d24af5e21da60b
YumaInaura <yuma.inaura@gmail.com>
2022-12-09 19:29:41 +0900

    migrate

--- django/blog/polls/models.py
+++ django/blog/polls/models.py
@@ -3,6 +3,9 @@ from django.db import models
 from django.utils import timezone
 import datetime
 
+class Article(models.Model):
+    article_title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
