b5ace7c66ea9cfd3de0f6c609fdb9dd886d3b9d8
YumaInaura <yuma.inaura@gmail.com>
2022-12-09 17:36:49 +0900

    tests

--- django-study/mysite/polls/admin.py
+++ django-study/mysite/polls/admin.py
@@ -14,5 +14,7 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
--- django-study/mysite/polls/models.py
+++ django-study/mysite/polls/models.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 import datetime
@@ -6,11 +7,11 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-    # @admin.display(
-    #     boolean=True,
-    #     ordering='pub_date',
-    #     description='Published recently?',
-    # )
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
