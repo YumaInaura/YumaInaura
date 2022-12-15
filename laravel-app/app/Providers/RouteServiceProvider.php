33e5d5e4f339167a4faa4b3c7e7239c3ee31ab14
YumaInaura <yuma.inaura@gmail.com>
2022-12-15 14:48:25 +0900

    greeting

--- laravel-app/app/Providers/RouteServiceProvider.php
+++ laravel-app/app/Providers/RouteServiceProvider.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+
 class RouteServiceProvider extends ServiceProvider
 {
     /**
