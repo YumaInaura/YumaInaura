8dd49137cbd23ebe69d7fff6222855414dadbae2
YumaInaura <yuma.inaura@gmail.com>
2022-12-16 14:05:16 +0900

    php artisan breeze:install

--- laravel-app/app/Providers/RouteServiceProvider.php
+++ laravel-app/app/Providers/RouteServiceProvider.php
@@ -28,7 +28,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/dashboard';
 
     /**
      * The controller namespace for the application.
