7f12b2aa7f392c7c847df72ccda2f4c81b002014
YumaInaura <yuma.inaura@gmail.com>
2022-12-17 16:19:48 +0900

    breeze

--- laravel9-vite/app/Providers/RouteServiceProvider.php
+++ laravel9-vite/app/Providers/RouteServiceProvider.php
@@ -32,7 +32,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/dashboard';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
--- laravel9-vite/resources/js/app.js
+++ laravel9-vite/resources/js/app.js
@@ -1,2 +1,7 @@
-// import './bootstrap';
-import '../css/app.css';
+import './bootstrap';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
