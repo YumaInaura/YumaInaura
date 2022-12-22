eed6b64cab9e800211777758b8ef5f6dd7095a01
YumaInaura <yuma.inaura@gmail.com>
2022-12-23 08:02:12 +0900

    phpunit

---
+++ phpunit/src/arithmetic.php
@@ -0,0 +1,18 @@
+<?php
+namespace App;
+class Arithmetic {
+
+    public function add($x,$y) {
+        return ($x+$y);
+    }
+    public function substract($x,$y) {
+        return ($x-$y);
+    }
+    public function multiply($x,$y) {
+        return ($x*$y);
+    }
+    public function divide($x,$y) {
+        return ($x/$y);
+    }
+}
+?>
