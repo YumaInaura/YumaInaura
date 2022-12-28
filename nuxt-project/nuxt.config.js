5b3c00e90b96cbac7df78944fb26cafc6635478a
YumaInaura <yuma.inaura@gmail.com>
2022-12-28 16:01:22 +0900

    nuxt

--- nuxt-project/nuxt.config.js
+++ nuxt-project/nuxt.config.js
@@ -35,6 +35,8 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    "@nuxtjs/axios",
+    'nuxt-browser-console',
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
--- nuxt-project/pages/index.vue
+++ nuxt-project/pages/index.vue
@@ -2,10 +2,21 @@
   <Tutorial/>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
+<script>
+import { console } from 'console';
 
-export default Vue.extend({
-  name: 'IndexPage'
-})
+	export default {
+		async asyncData({ $axios }) {
+			// 取得先のURL
+			const url = "https://example.com/";
+      // console.log("abc");
+			// リクエスト（Get）
+			const response = await $axios.$get(url);
+			// 配列で返ってくるのでJSONにして返却
+			return {
+				posts: response
+			};
+		}
+	};
 </script>
+
