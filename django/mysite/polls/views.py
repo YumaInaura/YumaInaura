c3743f2da75c3f6ee08d8f5d59c02fc54375ad0a
YumaInaura <yuma.inaura@gmail.com>
2022-12-28 15:22:08 +0900

    nuxt

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -7,14 +7,17 @@ from .models import Choice, Question
 
 from django.views.generic.edit import UpdateView
 
+from django.contrib.auth.decorators import login_required
+# @login_required
 class IndexView(generic.ListView):
+    login_url = '/login/'
+
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
-
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
