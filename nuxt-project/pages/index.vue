5ae62fa398c0f57e786336c1ad773b7424efcecb
YumaInaura <yuma.inaura@gmail.com>
2022-12-29 13:32:38 +0900

    try

--- nuxt-project/pages/index.vue
+++ nuxt-project/pages/index.vue
@@ -1,8 +1,11 @@
 <!-- https://develop365.gitlab.io/nuxtjs-2.8.X-doc/ja/api/configuration-loading/ -->
 
 <template>
-	<Tutorial />
+	<div>
+		<span v-for="n in 10000">{{ n }} </span>
+	</div>
 </template>
+
 
 <script>
 import Vue from 'vue'
@@ -11,12 +14,10 @@ export default Vue.extend({
   mounted () {
     this.$nextTick(() => {
       this.$nuxt.$loading.start()
-
-      setTimeout(() => this.$nuxt.$loading.finish(), 2000)
-    })
+			setTimeout(() => this.$nuxt.$loading.finish(), 500)
+		})
   },
 	created() {
-
 	},
 	methods(){},
 	async asyncData({ $axios }) {
