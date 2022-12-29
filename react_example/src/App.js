fe9300de3706b2fb507c8b91589ef86edaab56d1
YumaInaura <yuma.inaura@gmail.com>
2022-12-29 18:09:22 +0900

    router

--- react_example/src/App.js
+++ react_example/src/App.js
@@ -1,16 +1,22 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './Home';
-// import About from './routes/about';
-// import Contact from './routes/contact';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+function Hello() {
+  return <h2>Hello</h2>;
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <h1>Hello React Router v6</h1>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/hello" element={<Hello />} />
       </Routes>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- react_example/src/index.js
+++ react_example/src/index.js
@@ -4,21 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { BrowserRouter } from 'react-router-dom';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
