fdad0f24583a46bf68dbeacfd2d96b644602fbf9
YumaInaura <yuma.inaura@gmail.com>
2022-12-29 16:07:23 +0900

    react

--- react_example/src/App.js
+++ react_example/src/App.js
@@ -1,21 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+
+import React, { Component } from 'react';
 
 function App() {
-
   return (
     <BrowserRouter>
       <h1>Hello React Router</h1>
-      <Route path="/">
+      <Route path="/sasa">
         <Home />
       </Route>
     </BrowserRouter>
-  );
+    );
 }
 
-function Home() {
-  return <h2>Home</h2>;
+class Home extends Component {
+  render() {
+    return <h2>PageAです</h2>;
+  }
 }
 
 
