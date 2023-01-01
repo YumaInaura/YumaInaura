359fbf48f5b230ddeaac5e578b284244e501ee4a
YumaInaura <yuma.inaura@gmail.com>
2023-01-01 15:06:35 +0900

    vue

--- nuxt3-app/pages/example.vue
+++ nuxt3-app/pages/example.vue
@@ -1,5 +1,27 @@
+<!-- https://zenn.dev/coedo/articles/86bc31acb4ea47 -->
+
+<script lang="ts">
+export default defineComponent({
+  async setup() {
+    const { data: items } = await useFetch('https://jsonplaceholder.typicode.com/todos/')
+    return {
+      items
+    }
+  },
+})
+</script>
+
+<!--
+<script setup lang="ts">
+    const data = await fetch('https://jsonplaceholder.typicode.com/todos/')
+    const { data: items } = await data.json()
+    console.log(items)
+</script> -->
+
 <template>
-  <div>
-    Example
-  </div>
+  <ul>
+    <li v-for="item in items">
+      {{ item.id }} {{ item.title }}
+    </li>
+  </ul>
 </template>
