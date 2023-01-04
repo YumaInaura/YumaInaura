d86c2d84ad8f96ae9bec1afafe26e967cc5fc18d
YumaInaura <yuma.inaura@gmail.com>
2023-01-05 00:03:58 +0900

    update

--- django/mysite/mysite/settings.py
+++ django/mysite/mysite/settings.py
@@ -81,6 +81,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'polls.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
--- django/mysite/polls/models.py
+++ django/mysite/polls/models.py
@@ -1,10 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+
 from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 import datetime
 
-class Foo(models.Model):
-    pass
+class CustomUser(AbstractUser):
+    """拡張ユーザーモデル"""
+    class Meta:
+        verbose_name_plural = "CustomUser"
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
