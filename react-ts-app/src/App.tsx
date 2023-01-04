bd6e329999701bec69bbb0f4443cd452ba7e74b1
YumaInaura <yuma.inaura@gmail.com>
2023-01-04 16:44:14 +0900

    update

--- react-ts-app/src/App.tsx
+++ react-ts-app/src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+
+function Hello() {
+  return <h2>Hello</h2>;
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <h1>Hello React Router v6</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hello" element={<Hello />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.scss';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
