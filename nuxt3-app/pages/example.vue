e6c49026a99172f17214c341cc4d555dac3874ca
YumaInaura <yuma.inaura@gmail.com>
2023-01-08 23:20:58 +0900

    update

---
+++ nuxt3-app/pages/example.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <li v-for="item in items">
+      {{ item.id }} {{ item.title }}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  async setup() {
+    const items = []
+
+    const getTodos = async () => {
+      const data = await fetch('https://jsonplaceholder.typicode.com/todos/')
+      const json = await data.json()
+      this.items = json
+    }
+  },
+})
+</script>
--- nuxt3-app/pages/example2.vue
+++ nuxt3-app/pages/example2.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtPage />
+    AAAAAAAAA
   </div>
 </template>
