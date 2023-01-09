903cd01bdb2afdebdddf1fed4642cc1ff76513ee
YumaInaura <yuma.inaura@gmail.com>
2023-01-09 14:51:12 +0900

    update

--- next-ts-app/src/Example.tsx
+++ next-ts-app/src/Example.tsx
@@ -1,16 +1,30 @@
+// React CSSで悩む全ての人へ【2022年版】
+// https://ramble.impl.co.jp/1414/
+
+// ラジオボタンの選択肢を画像にする【CSSのみでOK】
+// https://mgmgblog.com/post-1935/
 
 import './App.scss';
+import axios from "axios";
+import FormCssModule from "./Form.module.css";
 
-import { SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
-import React from 'react'
+import { useForm } from "react-hook-form";
+import { useState } from 'react'
 
-const Example = () => {
+const Form = () => {
   const { register, handleSubmit } = useForm();
 
-  const items = ["アイテム１", "アイテム２", "アイテム３", "アイテム４"]
-  const [val, setVal] = React.useState('アイテム１');
+  const items = ["small", "large"]
+  const [val, setVal] = useState('item１');
 
-  const onSubmit = (data: any) => console.log(data);
+  const requestURL = "https://httpbin.org/post";
+  const [getData, setResponse] = useState({ form: { content: "", item: "" } });
+
+  const onSubmit = (data: any) => {
+    axios.post(requestURL, new URLSearchParams(data)).then((response) => {
+      setResponse(response.data);
+    });
+  };
   const handleChange = (e: any) => {
     setVal(e.target.value);
   };
@@ -25,25 +39,37 @@ const Example = () => {
         </div>
         <div>
         </div>
+        <div className={FormCssModule["bg"]}>
+AA
+              </div>
+
         {items.map((item) => {
           return (
             <div key={item}>
+              <label htmlFor={item} className={FormCssModule["item-label"]}>{item}</label>
               <input
-                id={item}
+                className={FormCssModule["item-input"]}
+                id="sizeSelectSmall"
                 type="radio"
                 {...register('item')}
                 value={item}
                 onChange={handleChange}
                 checked={item === val}
               />
-              <label htmlFor={item}>{item}</label>
             </div>
           );
         })}
         <button type="submit">Submit</button>
       </form>
+      <div>
+        <h2>Content</h2>
+        <p>{getData['form']['content']}</p>
+        <h2>Item</h2>
+        <p>{getData['form']['item']}</p>
+      </div>
     </div>
+
   );
 }
 
-export default Example;
+export default Form;
