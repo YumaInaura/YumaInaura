28bfe7661cd732462519d4d30dd9f7ae21cb58d1
YumaInaura <yuma.inaura@gmail.com>
2023-01-10 20:17:31 +0900

    update

---
+++ next-app/pages/uniontype.tsx
@@ -0,0 +1,47 @@
+export type SelectType = 'A' | 'B' | 'def';
+import React, { useState, useEffect } from 'react';
+
+export const UnionType = () => {
+  type ValueTypes = 'A' | 'B';
+
+  const [value, setValue] = useState<ValueTypes>();
+
+  const clickA = () => {
+    setValue("A")
+  }
+
+  const clickB = () => {
+    setValue("A")
+  }
+
+  // Error
+  // const clickC = () => {
+  //   setValue("C")
+  // }
+
+  useEffect(() => {
+    switch (value) {
+      case "A":
+        console.log("is A")
+        break
+      case "B":
+        console.log("is B")
+        break
+      // Error
+      // case "C":
+      //   console.log("is C")
+
+    };
+  });
+
+
+  return (
+    <div>
+      <button onClick={clickA}>ClickA</button>
+      <button onClick={clickB}>ClickB</button>
+    </div>
+  );
+
+};
+
+export default UnionType;
