5d298c752ae5fc21a24466c9afdda82472c513d9
YumaInaura <yuma.inaura@gmail.com>
2023-01-12 01:13:03 +0900

    update

--- next-app/pages/example.tsx
+++ next-app/pages/example.tsx
@@ -1,40 +1,16 @@
-import axios from "axios";
-import { useState } from 'react'
 
-const DiscoverOffice = () => {
-  const [val, setVal] = useState('');
-  const requestURL = "https://httpbin.org/post";
-  const [gotResponse, setResponse] = useState({ "form": { item: "" }});
+const Example = () => {
 
-  const HeatHandleChange = (data: any) => {
-    setVal(data.target.value);
-    axios.post(requestURL, new URLSearchParams({ item: data.target.value })).then((response) => {
-      setResponse(response.data);
-    });
+  const handleOnclick = (e: any) => {
+    console.log(e.target.textContent)
   }
 
   return (
     <>
       <div>
-        <input
-          type="radio"
-          value="heat"
-          onChange={HeatHandleChange}
-          checked={val === 'heat'}
-        />
-        Heat
+        <button onClick={handleOnclick}>Click me</button>
       </div>
-      <div>
-        <input
-          type="radio"
-          value="cool"
-          onChange={HeatHandleChange}
-          checked={val === 'cool'}
-        />
-        Cool
-      </div>
-      <div>{gotResponse['form']["item"]}</div>
     </>
   );
 }
-export default DiscoverOffice;
+export default Example;
