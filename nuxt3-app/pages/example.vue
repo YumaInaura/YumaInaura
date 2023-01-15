c4bf897b8d313a2865cd1641e67b6cb86ab2095e
YumaInaura <yuma.inaura@gmail.com>
2023-01-15 10:22:08 +0900

    update

--- nuxt3-app/pages/example.vue
+++ nuxt3-app/pages/example.vue
@@ -1,27 +1,11 @@
-<template>
-    <button @click="goHome">GO GO</button>
-</template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import { useRouter } from 'vue-router'
-
-export default defineComponent({
-  setup() {
-        const router = useRouter()
-
-    const goHome = async () => {
-        try {
-            router.push("/");
-        } catch (error) {
-            console.log("[signinError]", error);
-            alert(`NG:${error}`);
-        }
-    };
-
-    return {
-      goHome,
-    }
-  },
-})
+<script lang="ts" setup>
+const onClick = () => {
+  console.log("Button click");
+};
 </script>
+
+<template>
+  <div>
+    <button @click="onClick()">Button</button>
+  </div>
+</template>
--- rails-example/app/views/exes/index.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.array! @exes, partial: "exes/ex", as: :ex
