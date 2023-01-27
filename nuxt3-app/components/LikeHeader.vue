3bbc15f400d38ba1e34e78b68654b172187146bc
YumaInaura <yuma.inaura@gmail.com>
2023-01-27 17:41:08 +0900

    update

---
+++ nuxt3-app/components/LikeHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+      <p>Total Good</p>
+      <p>{{ showNum }}</p>
+  </div><!-- /.header -->
+</template>
+<script>
+export default {
+  props:{
+      totalNumber:{
+          type:Number,
+          default:10
+      }
+  },
+  computed:{
+      showNum(){
+          return(
+              this.totalNumber
+          )
+      }
+
+  }
+}
+</script>
---
+++ nuxt3-app/pages/IncrementParent.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <div>
+    <LikeHeader :totalNumber="number"></LikeHeader>
+    <LikeNumber @total-increment="increment"></LikeNumber>
+    <LikeNumber @total-increment="increment"></LikeNumber>
+    <button @click="reset">reset</button>
+  </div>
+</template>
+<script>
+import LikeHeader from '../components/LikeHeader.vue'
+export default {
+  data(){
+    return{
+      number:0
+    }
+  },
+  methods:{
+    increment(){
+      this.number += 1
+    },
+    reset(){
+      this.number = 0
+    }
+  },
+  components:{
+    LikeHeader
+  }
+}
+</script>
