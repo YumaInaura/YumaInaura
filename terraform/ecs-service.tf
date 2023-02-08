b2c74160e522d642a6a2f2ae953ad80e1c6927c0
YumaInaura <yuma.inaura@gmail.com>
2023-02-08 15:22:52 +0900

    update

--- terraform/ecs-service.tf
+++ terraform/ecs-service.tf
@@ -3,10 +3,11 @@ resource "aws_ecs_service" "ex" {
   cluster         = "ex"
   task_definition = "rails-task"
 
+  launch_type = "FARGATE"
   network_configuration {
     assign_public_ip = true
-    security_groups  = ["sg-1f7c0957"]
-    subnets          = ["subnet-b062c69b", "subnet-8e151ad5"]
+    security_groups  = ["sg-08c283ec45b140c3a"]
+    subnets          = ["subnet-06a89891663ab6b13", "subnet-0a1be7e1bcb530048"]
   }
 
   # desired_count   = 3
@@ -18,11 +19,11 @@ resource "aws_ecs_service" "ex" {
   #   field = "cpu"
   # }
 
-  # load_balancer {
-  #   target_group_arn = aws_lb_target_group.foo.arn
-  #   container_name   = "mongo"
-  #   container_port   = 8080
-  # }
+  load_balancer {
+    target_group_arn = "arn:aws:elasticloadbalancing:ap-northeast-1:842696858454:targetgroup/tg3435/b61e6c63da5b6c4e"
+    container_name   = "mongo"
+    container_port   = 80
+  }
 
   # placement_constraints {
   #   type       = "memberOf"
