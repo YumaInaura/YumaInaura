7b74ca2c1f4b228c3c640cf259d9461123bbc62f
YumaInaura <yuma.inaura@gmail.com>
2023-02-21 23:34:17 +0900

    update

---
+++ nuxt3-app/pages/fetchchild.vue
@@ -0,0 +1,33 @@
+<template>
+  <h2>FetchChild</h2>
+  <div>{{ response }}</div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  response: Object
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  response: {},
+});
+
+watch(() => props.response, (first, second) => {
+  console.log(
+    "Watch props.selected function called with args:",
+    first,
+    second
+  );
+});
+
+// // const responseJson = ref(props.responseJson)
+// export default{
+//   props: {
+//     responseJson: {
+//       type: Object,
+//       required: true,
+//       default: {}
+//     }
+//   }
+// }
+</script>
--- nuxt3-app/pages/fetchparent.vue
+++ nuxt3-app/pages/fetchparent.vue
@@ -1,19 +1,27 @@
 
 <template>
-  <h1>Todo</h1>
-  <div>
-    {{ todoItem.title }}
-  </div>
+  <h1>Fetch – Response to Child</h1>
+  <FetchChild  :response="response"/>
 </template>
 
-<script setup lang="ts">
-const todoItem = ref({})
+<script lang="ts" setup>
+import FetchChild from "./fetchchild.vue"
 
-async function selectTodo() {
-  const data = await fetch(`https://jsonplaceholder.typicode.com/todos/1`)
-  const json = await data.json()
-  todoItem.value = json
+const response = ref()
+const responseJson = ref()
+
+function fetchTodo() {
+  fetch("https://jsonplaceholder.typicode.com/todos/1", {
+    method: "GET",
+  })
+    .then((response) => {
+      return response
+    })
+    .then((data) => {
+      console.log(data)
+      response.value = data
+    })
 }
-selectTodo()
+fetchTodo()
 
 </script>
