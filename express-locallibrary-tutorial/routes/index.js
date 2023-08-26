d77cae2bbaa14b6eb6ca55c63bec01a20ecddd20
YumaInaura <yuma.inaura@gmail.com>
2023-08-26 21:31:36 +0900

    express

--- express-locallibrary-tutorial/routes/index.js
+++ express-locallibrary-tutorial/routes/index.js
@@ -3,7 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Expresssssss' });
 });
 
 module.exports = router;
+
+//Import the mongoose module
+var mongoose = require("mongoose");
+
+//Set up default mongoose connection
+var mongoDB = "mongodb://127.0.0.1/my_database";
+mongoose.connect(mongoDB);
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// Define schema
+var Schema = mongoose.Schema;
+
+var SomeModelSchema = new Schema({
+  a_string: String,
+  a_date: Date,
+});
+
+// Compile model from schema
+var SomeModel = mongoose.model("SomeModel", SomeModelSchema);
+
