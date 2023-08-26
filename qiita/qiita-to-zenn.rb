af1db568b9eaa58984b554056a09da661205361a
YumaInaura <yuma.inaura@gmail.com>
2023-08-26 12:41:56 +0900

    update

--- qiita/qiita-to-zenn.rb
+++ qiita/qiita-to-zenn.rb
@@ -1,8 +1,6 @@
-require 'securerandom'
 require 'net/https'
 require 'uri'
 require 'json'
-require 'pry'
 
 uri = URI.parse("https://qiita.com/")
 http = Net::HTTP.new(uri.host, uri.port)
@@ -12,6 +10,7 @@ request_header = {'Content-Type' =>'application/json', "Authorization" => "Beare
 
 round = 0
 
+# ページングしながらQiitaの全記事を取得
 (1..100).each do |i|
   get_url = "https://qiita.com/api/v2/users/#{ENV['USER_ID']}/items?page=#{i}&per_page=100"
 
@@ -33,23 +32,26 @@ round = 0
 
     omitted_title = item['title'].slice(0..69)
 
+    # 複数回実行しても記事が重複しないようにQiitaの記事作成日時をslugとして利用する
     slug = item['created_at'].gsub(':', '_').gsub('+', '-').gsub('T', 't')
 
     filepath = "../articles/qiita-#{slug}.md"
 
     puts filepath
 
+    # Qiitaのタグをそのままzennで使う
     tag_names = item['tags'].map { |tag| tag['name'] }
 
+    # Qiitaでポエムタグがついている記事はideaに、そうでない記事はtechに分類する
     type = if tag_names.include?('ポエム')
-      'tech'
-    else
       'idea'
+    else
+      'tech'
     end
 
     filebody = <<~EOM
     ---
-    title: "#{omitted_title}"
+    title: #{omitted_title.to_json}
     emoji: "🖥"
     type: "#{type}"
     topics: #{tag_names}
